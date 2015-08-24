5869f737fdbdd2e643e6a6a31e22cab1615860f2
Ruben Taelman <rubensworks@gmail.com>
2015-08-24 20:23:32 +0200

    Correctly drop inventory when display part is removed

--- src/main/java/org/cyclops/integrateddynamics/part/PartTypeDisplay.java
+++ src/main/java/org/cyclops/integrateddynamics/part/PartTypeDisplay.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import net.minecraft.block.Block;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.inventory.Container;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import org.apache.logging.log4j.Level;
 import org.cyclops.cyclopscore.config.extendedconfig.BlockConfig;
@@ -22,6 +23,8 @@ import org.cyclops.integrateddynamics.core.part.PartTarget;
 import org.cyclops.integrateddynamics.core.part.PartTypeBase;
 import org.cyclops.integrateddynamics.inventory.container.ContainerPartDisplay;
 
+import java.util.List;
+
 /**
  * A part that can display variables..
  * @author rubensworks
@@ -35,6 +38,31 @@ public class PartTypeDisplay extends PartTypeBase<PartTypeDisplay, PartTypeDispl
     @Override
     public Class<? super PartTypeDisplay> getPartTypeClass() {
         return PartTypeDisplay.class;
+    }
+
+    @Override
+    public void addDrops(PartTarget target, PartTypeDisplay.State state, List<ItemStack> itemStacks) {
+        for(int i = 0; i < state.getInventory().getSizeInventory(); i++) {
+            ItemStack itemStack = state.getInventory().getStackInSlot(i);
+            if(itemStack != null) {
+                itemStacks.add(itemStack);
+            }
+        }
+        state.getInventory().clear();
+        state.refresh(this, target);
+        super.addDrops(target, state, itemStacks);
+    }
+
+    @Override
+    public void beforeNetworkKill(Network network, PartTarget target, PartTypeDisplay.State state) {
+        super.beforeNetworkKill(network, target, state);
+        state.refresh(this, target);
+    }
+
+    @Override
+    public void afterNetworkAlive(Network network, PartTarget target, PartTypeDisplay.State state) {
+        super.afterNetworkAlive(network, target, state);
+        state.refresh(this, target);
     }
 
     @Override
