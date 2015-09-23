2f5dbcc44d46d384d983fd3c162fae25802496ac
Ruben Taelman <rubensworks@gmail.com>
2015-09-23 17:56:47 +0200

    Bump minimum Cyclops Core version to 0.2.0

--- src/main/java/org/cyclops/integrateddynamics/Reference.java
+++ src/main/java/org/cyclops/integrateddynamics/Reference.java
@@ -17,11 +17,15 @@ public final class Reference {
     public static final String VERSION_URL = "https://raw.githubusercontent.com/CyclopsMC/Versions/master/1.8/IntegratedDynamics.txt";
 
     // MOD ID's
+    public static final String MOD_FORGE = "Forge";
+    public static final String MOD_FORGE_VERSION = "@FORGE_VERSION@";
+    public static final String MOD_FORGE_VERSION_MIN = "11.14.3.1494";
     public static final String MOD_CYCLOPSCORE = "cyclopscore";
-    public static final String MOD_CYCLOPSCORE_MINVERSION = "@CYCLOPSCORE_VERSION@";
+    public static final String MOD_CYCLOPSCORE_VERSION = "@CYCLOPSCORE_VERSION@";
+    public static final String MOD_CYCLOPSCORE_VERSION_MIN = "0.2.0";
 
     public static final String MOD_DEPENDENCIES =
-              "required-after:Forge@[11.14.1.1329,);"
-            + "required-after:" + Reference.MOD_CYCLOPSCORE + ""; // TODO: add min version requirements
+            "required-after:" + MOD_FORGE       + "@[" + MOD_FORGE_VERSION_MIN       + ",);" +
+            "required-after:" + MOD_CYCLOPSCORE + "@[" + MOD_CYCLOPSCORE_VERSION_MIN + ",);";
 
 }
