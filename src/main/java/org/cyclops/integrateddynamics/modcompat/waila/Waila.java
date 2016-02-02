6439faedecdd2de9529fa7915ca52a4bbe55f6a6
Ruben Taelman <rubensworks@gmail.com>
2016-02-02 21:07:10 +0100

    Add proxy id's to Waila

--- src/main/java/org/cyclops/integrateddynamics/modcompat/waila/Waila.java
+++ src/main/java/org/cyclops/integrateddynamics/modcompat/waila/Waila.java
@@ -5,6 +5,7 @@ import mcp.mobius.waila.api.IWailaRegistrar;
 import org.cyclops.cyclopscore.helper.L10NHelpers;
 import org.cyclops.integrateddynamics.Reference;
 import org.cyclops.integrateddynamics.api.part.IPartContainerFacade;
+import org.cyclops.integrateddynamics.tileentity.TileProxy;
 
 /**
  * Waila support class.
@@ -19,16 +20,26 @@ public class Waila {
      */
     public static void callbackRegister(IWailaRegistrar registrar){
         registrar.addConfig(Reference.MOD_NAME, getPartConfigId(), L10NHelpers.localize("gui." + Reference.MOD_ID + ".waila.partConfig"));
+        registrar.addConfig(Reference.MOD_NAME, getProxyConfigId(), L10NHelpers.localize("gui." + Reference.MOD_ID + ".waila.proxyConfig"));
         registrar.registerBodyProvider(new PartDataProvider(), IPartContainerFacade.class);
         registrar.registerBodyProvider(new PartDataProvider(), IMultipartContainer.class);
+        registrar.registerBodyProvider(new ProxyDataProvider(), TileProxy.class);
     }
     
     /**
-     * Config ID.
+     * Part config ID.
      * @return The config ID.
      */
     public static String getPartConfigId() {
-        return Reference.MOD_ID + ".tank";
+        return Reference.MOD_ID + ".part";
+    }
+
+    /**
+     * Proxy config ID.
+     * @return The config ID.
+     */
+    public static String getProxyConfigId() {
+        return Reference.MOD_ID + ".proxy";
     }
     
 }
