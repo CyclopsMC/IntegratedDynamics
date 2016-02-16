3712ecbf3551d29b6cd5884d84fb31e00f9e068b
Ruben Taelman <rubensworks@gmail.com>
2016-02-16 19:34:28 +0100

    Add Waila support for the Drying Basin

---
+++ src/main/java/org/cyclops/integrateddynamics/modcompat/waila/DryingBasinDataProvider.java
@@ -0,0 +1,64 @@
+package org.cyclops.integrateddynamics.modcompat.waila;
+
+import mcp.mobius.waila.api.IWailaConfigHandler;
+import mcp.mobius.waila.api.IWailaDataAccessor;
+import mcp.mobius.waila.api.IWailaDataProvider;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.World;
+import org.cyclops.cyclopscore.helper.L10NHelpers;
+import org.cyclops.cyclopscore.helper.TileHelpers;
+import org.cyclops.integrateddynamics.Reference;
+import org.cyclops.integrateddynamics.tileentity.TileDryingBasin;
+
+import java.util.List;
+
+/**
+ * Waila data provider for the drying basin.
+ * @author rubensworks
+ *
+ */
+public class DryingBasinDataProvider implements IWailaDataProvider {
+
+    @Override
+    public ItemStack getWailaStack(IWailaDataAccessor accessor, IWailaConfigHandler config) {
+        return null;
+    }
+
+    @Override
+    public List<String> getWailaHead(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
+        return currenttip;
+    }
+
+    @Override
+    public List<String> getWailaTail(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
+        return currenttip;
+    }
+
+    @Override
+    public List<String> getWailaBody(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
+        if(config.getConfig(Waila.getProxyConfigId())) {
+            TileDryingBasin tile = TileHelpers.getSafeTile(accessor.getWorld(), accessor.getPosition(), TileDryingBasin.class);
+            if (tile != null) {
+                if(tile.getStackInSlot(0) != null) {
+                    currenttip.add(L10NHelpers.localize("gui." + Reference.MOD_ID + ".waila.item",
+                            tile.getStackInSlot(0).getDisplayName()));
+                }
+                if(!tile.getTank().isEmpty()) {
+                    currenttip.add(L10NHelpers.localize("gui." + Reference.MOD_ID + ".waila.fluid",
+                            tile.getTank().getFluid().getLocalizedName(), tile.getTank().getFluidAmount()));
+                }
+            }
+        }
+        return currenttip;
+    }
+
+    @Override
+    public NBTTagCompound getNBTData(EntityPlayerMP player, TileEntity te, NBTTagCompound tag, World world, BlockPos pos) {
+        return tag;
+    }
+
+}
--- src/main/java/org/cyclops/integrateddynamics/modcompat/waila/Waila.java
+++ src/main/java/org/cyclops/integrateddynamics/modcompat/waila/Waila.java
@@ -5,6 +5,7 @@ import mcp.mobius.waila.api.IWailaRegistrar;
 import org.cyclops.cyclopscore.helper.L10NHelpers;
 import org.cyclops.integrateddynamics.Reference;
 import org.cyclops.integrateddynamics.api.part.IPartContainerFacade;
+import org.cyclops.integrateddynamics.tileentity.TileDryingBasin;
 import org.cyclops.integrateddynamics.tileentity.TileProxy;
 
 /**
@@ -21,9 +22,11 @@ public class Waila {
     public static void callbackRegister(IWailaRegistrar registrar){
         registrar.addConfig(Reference.MOD_NAME, getPartConfigId(), L10NHelpers.localize("gui." + Reference.MOD_ID + ".waila.partConfig"));
         registrar.addConfig(Reference.MOD_NAME, getProxyConfigId(), L10NHelpers.localize("gui." + Reference.MOD_ID + ".waila.proxyConfig"));
+        registrar.addConfig(Reference.MOD_NAME, getDryingBasinConfigId(), L10NHelpers.localize("gui." + Reference.MOD_ID + ".waila.dryingBasinConfig"));
         registrar.registerBodyProvider(new PartDataProvider(), IPartContainerFacade.class);
         registrar.registerBodyProvider(new PartDataProvider(), IMultipartContainer.class);
         registrar.registerBodyProvider(new ProxyDataProvider(), TileProxy.class);
+        registrar.registerBodyProvider(new DryingBasinDataProvider(), TileDryingBasin.class);
     }
     
     /**
@@ -40,6 +43,14 @@ public class Waila {
      */
     public static String getProxyConfigId() {
         return Reference.MOD_ID + ".proxy";
+    }
+
+    /**
+     * Proxy config ID.
+     * @return The config ID.
+     */
+    public static String getDryingBasinConfigId() {
+        return Reference.MOD_ID + ".dryingBasin";
     }
     
 }
