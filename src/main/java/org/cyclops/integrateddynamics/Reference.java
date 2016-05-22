04d400736e624e376a3bce706431bca6924c98b4
Ruben Taelman <rubensworks@gmail.com>
2016-05-22 15:52:11 +0200

    Add changelog

--- src/main/java/org/cyclops/integrateddynamics/Reference.java
+++ src/main/java/org/cyclops/integrateddynamics/Reference.java
@@ -31,7 +31,7 @@ public final class Reference {
     public static final String MOD_FORGE_VERSION_MIN = "12.17.0.1909";
     public static final String MOD_CYCLOPSCORE = "cyclopscore";
     public static final String MOD_CYCLOPSCORE_VERSION = "@CYCLOPSCORE_VERSION@";
-    public static final String MOD_CYCLOPSCORE_VERSION_MIN = "0.6.0";
+    public static final String MOD_CYCLOPSCORE_VERSION_MIN = "0.6.1";
     public static final String MOD_CHARSETPIPES = "CharsetPipes";
     public static final String MOD_MCMULTIPART = "mcmultipart";
     public static final String MOD_WAILA = "Waila";
