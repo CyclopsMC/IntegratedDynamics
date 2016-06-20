f7d1d4313dae19d5abdbf283b78f0f522e518863
Ruben Taelman <rubensworks@gmail.com>
2016-06-20 21:22:50 +0200

    Add TCon recipe registration safe for when TCon is not available

--- src/main/java/org/cyclops/integrateddynamics/modcompat/tconstruct/TConstructModCompat.java
+++ src/main/java/org/cyclops/integrateddynamics/modcompat/tconstruct/TConstructModCompat.java
@@ -1,14 +1,7 @@
 package org.cyclops.integrateddynamics.modcompat.tconstruct;
 
-import net.minecraft.item.ItemStack;
 import org.cyclops.cyclopscore.modcompat.IModCompat;
-import org.cyclops.cyclopscore.recipe.custom.api.IRecipe;
-import org.cyclops.cyclopscore.recipe.custom.component.DurationRecipeProperties;
-import org.cyclops.cyclopscore.recipe.custom.component.ItemAndFluidStackRecipeComponent;
 import org.cyclops.integrateddynamics.Reference;
-import org.cyclops.integrateddynamics.block.BlockCrystalizedMenrilBlockConfig;
-import org.cyclops.integrateddynamics.block.BlockDryingBasin;
-import slimeknights.tconstruct.library.TinkerRegistry;
 
 /**
  * Compatibility plugin for Tinkers' Construct.
@@ -25,13 +18,7 @@ public class TConstructModCompat implements IModCompat {
     @Override
     public void onInit(Step step) {
     	if(step == Step.POSTINIT) {
-            IRecipe<ItemAndFluidStackRecipeComponent, ItemAndFluidStackRecipeComponent, DurationRecipeProperties> recipe = BlockDryingBasin.getInstance().getRecipeRegistry().findRecipeByOutput(
-                    new ItemAndFluidStackRecipeComponent(
-                            new ItemStack(BlockCrystalizedMenrilBlockConfig._instance.getBlockInstance()), null));
-            if (recipe != null) {
-                TinkerRegistry.registerBasinCasting(recipe.getOutput().getItemStack(), null,
-                        recipe.getInput().getFluidStack().getFluid(), recipe.getInput().getFluidStack().amount);
-            }
+            TConstructRecipeManager.register();
     	}
     }
     
