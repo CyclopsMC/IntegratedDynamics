166d6689368e1a14951227c73c2c50af4aefbe8a
Ruben Taelman <rubensworks@gmail.com>
2016-06-21 19:16:04 +0200

    Delombokize Variable to fix compilation error on Travis

--- src/main/java/org/cyclops/integrateddynamics/core/evaluate/variable/Variable.java
+++ src/main/java/org/cyclops/integrateddynamics/core/evaluate/variable/Variable.java
@@ -1,6 +1,6 @@
 package org.cyclops.integrateddynamics.core.evaluate.variable;
 
-import lombok.Data;
+import org.cyclops.integrateddynamics.api.evaluate.EvaluationException;
 import org.cyclops.integrateddynamics.api.evaluate.variable.IValue;
 import org.cyclops.integrateddynamics.api.evaluate.variable.IValueType;
 import org.cyclops.integrateddynamics.api.evaluate.variable.IVariable;
@@ -9,10 +9,23 @@ import org.cyclops.integrateddynamics.api.evaluate.variable.IVariable;
  * A default variable implementation.
  * @author rubensworks
  */
-@Data
 public class Variable<V extends IValue> implements IVariable<V> {
 
     private final IValueType<V> type;
     private final V value;
 
+    public Variable(IValueType<V> type, V value) {
+        this.type = type;
+        this.value = value;
+    }
+
+    @Override
+    public IValueType<V> getType() {
+        return type;
+    }
+
+    @Override
+    public V getValue() throws EvaluationException {
+        return value;
+    }
 }
