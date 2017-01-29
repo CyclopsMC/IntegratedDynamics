62f61cc8fddc4775423ef580081452569e4c56b6
Ruben Taelman <rubensworks@gmail.com>
2017-01-29 15:55:40 +0100

    Bump mod version

--- src/main/java/org/cyclops/integrateddynamics/Reference.java
+++ src/main/java/org/cyclops/integrateddynamics/Reference.java
@@ -32,7 +32,7 @@ public final class Reference {
     public static final String MOD_FORGE_VERSION_MIN = "13.20.0.2201";
     public static final String MOD_CYCLOPSCORE = "cyclopscore";
     public static final String MOD_CYCLOPSCORE_VERSION = "@CYCLOPSCORE_VERSION@";
-    public static final String MOD_CYCLOPSCORE_VERSION_MIN = "0.9.1";
+    public static final String MOD_CYCLOPSCORE_VERSION_MIN = "0.9.2";
     public static final String MOD_COMMONCAPABILITIES = "commoncapabilities";
     public static final String MOD_COMMONCAPABILITIES_VERSION_MIN = "1.3.0";
     public static final String MOD_CHARSETPIPES = "charsetpipes";
