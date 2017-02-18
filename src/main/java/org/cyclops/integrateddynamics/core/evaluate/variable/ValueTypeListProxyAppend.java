364caffcc0ec2d999d47b33f1a05fd9f7ea67d0a
Ruben Taelman <rubensworks@gmail.com>
2017-02-18 21:56:16 +0100

    Fix list append operator serialization

--- src/main/java/org/cyclops/integrateddynamics/core/evaluate/variable/ValueTypeListProxyAppend.java
+++ src/main/java/org/cyclops/integrateddynamics/core/evaluate/variable/ValueTypeListProxyAppend.java
@@ -1,9 +1,13 @@
 package org.cyclops.integrateddynamics.core.evaluate.variable;
 
+import net.minecraft.nbt.JsonToNBT;
+import net.minecraft.nbt.NBTException;
+import net.minecraft.nbt.NBTTagCompound;
 import org.cyclops.integrateddynamics.api.evaluate.EvaluationException;
 import org.cyclops.integrateddynamics.api.evaluate.variable.IValue;
 import org.cyclops.integrateddynamics.api.evaluate.variable.IValueType;
 import org.cyclops.integrateddynamics.api.evaluate.variable.IValueTypeListProxy;
+import org.cyclops.integrateddynamics.api.evaluate.variable.IValueTypeListProxyFactoryTypeRegistry;
 
 /**
  * An appended list.
@@ -16,7 +20,7 @@ public class ValueTypeListProxyAppend<T extends IValueType<V>, V extends IValue>
     private final V value;
 
     public ValueTypeListProxyAppend(IValueTypeListProxy<T, V> list, V value) {
-        super(list.getName(), list.getValueType());
+        super(ValueTypeListProxyFactories.APPEND.getName(), list.getValueType());
         this.list = list;
         this.value = value;
     }
@@ -35,5 +39,36 @@ public class ValueTypeListProxyAppend<T extends IValueType<V>, V extends IValue>
             return value;
         }
         return null;
+    }
+
+    public static class Factory implements IValueTypeListProxyFactoryTypeRegistry.IProxyFactory<IValueType<IValue>, IValue, ValueTypeListProxyAppend<IValueType<IValue>, IValue>> {
+
+        @Override
+        public String getName() {
+            return "append";
+        }
+
+        @Override
+        public String serialize(ValueTypeListProxyAppend<IValueType<IValue>, IValue> values) throws IValueTypeListProxyFactoryTypeRegistry.SerializationException {
+            NBTTagCompound tag = new NBTTagCompound();
+            tag.setString("valueType", values.value.getType().getUnlocalizedName());
+            tag.setString("value", values.value.getType().serialize(values.value));
+            tag.setString("sublist", ValueTypeListProxyFactories.REGISTRY.serialize(values.list));
+            return tag.toString();
+        }
+
+        @Override
+        public ValueTypeListProxyAppend<IValueType<IValue>, IValue> deserialize(String data) throws IValueTypeListProxyFactoryTypeRegistry.SerializationException {
+            try {
+                NBTTagCompound tag = JsonToNBT.getTagFromJson(data);
+                IValueType valueType = ValueTypes.REGISTRY.getValueType(tag.getString("valueType"));
+                IValue value = valueType.deserialize(tag.getString("value"));
+                IValueTypeListProxy list = ValueTypeListProxyFactories.REGISTRY.deserialize(tag.getString("sublist"));
+                return new ValueTypeListProxyAppend<>(list, value);
+            } catch (NBTException e) {
+                e.printStackTrace();
+                throw new IValueTypeListProxyFactoryTypeRegistry.SerializationException(e.getMessage());
+            }
+        }
     }
 }
