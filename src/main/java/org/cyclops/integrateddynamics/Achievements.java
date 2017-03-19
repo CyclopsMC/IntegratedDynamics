56174fb0bf4e86ab59ba4802b673ef0cb6acb33b
Ruben Taelman <rubensworks@gmail.com>
2017-03-19 18:47:48 +0100

    Add challenges tutorial

--- src/main/java/org/cyclops/integrateddynamics/Achievements.java
+++ src/main/java/org/cyclops/integrateddynamics/Achievements.java
@@ -1,6 +1,8 @@
 package org.cyclops.integrateddynamics;
 
 import com.google.common.base.Predicate;
+import net.minecraft.block.BlockSponge;
+import net.minecraft.entity.monster.EntityCreeper;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
@@ -78,6 +80,12 @@ public class Achievements {
 	public static final Achievement DYNAMIC_ADDITIONS = new ExtendedAchievement("dynamicAdditions", 8, 1, new ItemStack(ConfigHandler.isEnabled(ItemVariableConfig.class) ? ItemVariable.getInstance() : Items.APPLE), LOGIC_PROGRAMMING);
 	public static final Achievement DYNAMIC_LIST_FILTERING = new ExtendedAchievement("dynamicListFiltering", 9, 1, new ItemStack(ConfigHandler.isEnabled(ItemVariableConfig.class) ? ItemVariable.getInstance() : Items.APPLE), LOGIC_PROGRAMMING);
 
+	public static final Achievement CREEPER_TAMING = new ExtendedAchievement("creeperTaming", 8, 2, new ItemStack(Items.SKULL, 1, 4), WHAT_WOULD_I_BE_LOOKING_AT).setSpecial();
+	public static final Achievement SPONGE_STEP_SOUND = new ExtendedAchievement("spongeStepSound", 8, 3, new ItemStack(Blocks.SPONGE, 1, 1), WHAT_WOULD_I_BE_LOOKING_AT).setSpecial();
+	public static final Achievement RECURSIVE_RECURSION = new ExtendedAchievement("recursiveRecursion", 6, 2, new ItemStack(ConfigHandler.isEnabled(ItemVariableConfig.class) ? ItemVariable.getInstance() : Items.APPLE), WHAT_WOULD_I_BE_LOOKING_AT).setSpecial();
+	public static final Achievement TOOL_FOR_OBSIDIAN = new ExtendedAchievement("toolForObsidian", 6, 3, new ItemStack(Blocks.OBSIDIAN), WHAT_WOULD_I_BE_LOOKING_AT).setSpecial();
+	public static final Achievement SMART_PRESSURE_PLATE = new ExtendedAchievement("smartPressurePlate", 7, 4, new ItemStack(Blocks.HEAVY_WEIGHTED_PRESSURE_PLATE), WHAT_WOULD_I_BE_LOOKING_AT).setSpecial();
+
     private static final Achievement[] ACHIEVEMENTS = {
 			MENEGLIN_DISCOVERY,
 			SQUEEZING,
@@ -112,7 +120,13 @@ public class Achievements {
 			WHAT_WOULD_I_BE_LOOKING_AT,
 
 			DYNAMIC_ADDITIONS,
-			DYNAMIC_LIST_FILTERING
+			DYNAMIC_LIST_FILTERING,
+
+			CREEPER_TAMING,
+			SPONGE_STEP_SOUND,
+			RECURSIVE_RECURSION,
+			TOOL_FOR_OBSIDIAN,
+			SMART_PRESSURE_PLATE
 	};
 
 	private Achievements() {
@@ -216,6 +230,37 @@ public class Achievements {
 							event.getEntityPlayer().addStat(DYNAMIC_LIST_FILTERING);
                         }
 					} catch (EvaluationException e) {}
+				} else if (((LazyExpression) event.getVariable()).getOperator() == Operators.OBJECT_ENTITY_HEALTH) {
+					try {
+						IValue value0 = ((LazyExpression) event.getVariable()).getInput()[0].getValue();
+						if (value0.getType() == ValueTypes.OBJECT_ENTITY
+								&& ((ValueObjectTypeEntity.ValueEntity) value0).getRawValue().isPresent()
+								&& ((ValueObjectTypeEntity.ValueEntity) value0).getRawValue().get() instanceof EntityCreeper) {
+							event.getEntityPlayer().addStat(CREEPER_TAMING);
+						}
+					} catch (EvaluationException e) {}
+				} else if (((LazyExpression) event.getVariable()).getOperator() == Operators.OBJECT_ITEMSTACK_CAN_HARVEST_BLOCK) {
+					try {
+						IValue value0 = ((LazyExpression) event.getVariable()).getInput()[0].getValue();
+						IValue value1 = ((LazyExpression) event.getVariable()).getInput()[1].getValue();
+						if (value0.getType() == ValueTypes.OBJECT_ITEMSTACK
+								&& ((ValueObjectTypeItemStack.ValueItemStack) value0).getRawValue().isPresent()
+								&& ((ValueObjectTypeItemStack.ValueItemStack) value0).getRawValue().get().getItem() == Items.STONE_PICKAXE
+								&& value1.getType() == ValueTypes.OBJECT_BLOCK
+								&& ((ValueObjectTypeBlock.ValueBlock) value1).getRawValue().isPresent()
+								&& ((ValueObjectTypeBlock.ValueBlock) value1).getRawValue().get().getBlock() == Blocks.OBSIDIAN) {
+							event.getEntityPlayer().addStat(TOOL_FOR_OBSIDIAN);
+						}
+					} catch (EvaluationException e) {}
+				} else if (event.getVariable().getType() == ValueTypes.LIST) {
+					try {
+						IValue value0 = event.getVariable().getValue();
+						if (((ValueTypeList.ValueList) value0).getRawValue().isInfinite()) {
+							event.getEntityPlayer().addStat(RECURSIVE_RECURSION);
+						}
+					} catch (EvaluationException e) {
+						e.printStackTrace();
+					}
 				}
 			} else if (event.getVariable() instanceof Variable
 					&& event.getVariable().getType() == ValueTypes.LIST) {
@@ -227,6 +272,8 @@ public class Achievements {
 							&& ((ValueTypeInteger.ValueInteger) list.get(1)).getRawValue() == 10
 							&& ((ValueTypeInteger.ValueInteger) list.get(2)).getRawValue() == 100) {
 						event.getEntityPlayer().addStat(LOGICAL_LIST_BUILDING);
+					} else if (list.isInfinite()) {
+						event.getEntityPlayer().addStat(RECURSIVE_RECURSION);
 					}
 				} catch (EvaluationException e) {}
 			}
@@ -238,14 +285,50 @@ public class Achievements {
 		try {
 			IVariable variable = ((IPartStateWriter) event.getPartState()).getVariable(event.getPartNetwork());
 			if (event.getPartType() == PartTypes.REDSTONE_WRITER
-					&& ((event.getAspect() == Aspects.Write.Redstone.INTEGER
+					&& event.getEntityPlayer() != null) {
+				if (((event.getAspect() == Aspects.Write.Redstone.INTEGER
 						&& variable.getValue() instanceof ValueTypeInteger.ValueInteger
 						&& ((ValueTypeInteger.ValueInteger) variable.getValue()).getRawValue() >= 15)
-					|| (event.getAspect() == Aspects.Write.Redstone.INTEGER)
+						|| (event.getAspect() == Aspects.Write.Redstone.INTEGER)
 						&& variable.getValue() instanceof ValueTypeBoolean.ValueBoolean
-						&& ((ValueTypeBoolean.ValueBoolean) variable.getValue()).getRawValue())
-					&& event.getEntityPlayer() != null) {
-				event.getEntityPlayer().addStat(REDSTONE_TRANSMISSION);
+						&& ((ValueTypeBoolean.ValueBoolean) variable.getValue()).getRawValue())) {
+					event.getEntityPlayer().addStat(REDSTONE_TRANSMISSION);
+				} else if (event.getAspect() == Aspects.Write.Redstone.BOOLEAN
+						&& variable.getValue() instanceof ValueTypeBoolean.ValueBoolean
+						&& variable instanceof LazyExpression
+						&& ((LazyExpression) variable).getOperator() == Operators.RELATIONAL_EQUALS) {
+					IVariable var0 = ((LazyExpression) variable).getInput()[0];
+					IVariable var1 = ((LazyExpression) variable).getInput()[1];
+					IVariable varName, varCheck;
+					// Check which of the variables in the == operator is used for the static and dynamic name
+					if (var0 instanceof Variable) {
+						varName = var0;
+						varCheck = var1;
+					} else {
+						varName = var1;
+						varCheck = var0;
+					}
+					// The player name must equal the one of the current player,
+					// and the other variable must be read using an entity reader.
+					if (varName.getType() == ValueTypes.STRING
+							&& event.getEntityPlayer().getName()
+								.equals(((ValueTypeString.ValueString) varName.getValue()).getRawValue())
+							&& varCheck instanceof LazyExpression
+							&& ((LazyExpression) varCheck).getInput()[0] instanceof IAspectVariable
+							&& ((IAspectVariable) ((LazyExpression) varCheck).getInput()[0]).getAspect() == Aspects.Read.Entity.ENTITY) {
+						event.getEntityPlayer().addStat(SMART_PRESSURE_PLATE);
+					}
+				}
+			} else if (event.getPartType() == PartTypes.AUDIO_WRITER
+					&& event.getAspect() == Aspects.Write.Audio.STRING_SOUND
+					&& event.getEntityPlayer() != null
+					&& ((LazyExpression) variable).getOperator() == Operators.OBJECT_BLOCK_STEPSOUND) {
+				IValue value0 = ((LazyExpression) variable).getInput()[0].getValue();
+				if (value0.getType() == ValueTypes.OBJECT_BLOCK
+						&& ((ValueObjectTypeBlock.ValueBlock) value0).getRawValue().isPresent()
+						&& ((ValueObjectTypeBlock.ValueBlock) value0).getRawValue().get().getValue(BlockSponge.WET)) {
+					event.getEntityPlayer().addStat(SPONGE_STEP_SOUND);
+				}
 			}
 		} catch (EvaluationException e) {
 
