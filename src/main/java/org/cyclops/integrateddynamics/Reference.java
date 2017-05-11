9ed6c50193cce216f4a7304b8f5ff8a4c8c6e6e2
Ruben Taelman <rubensworks@gmail.com>
2017-05-11 17:36:34 +0200

    Bump Forge version to 12.18.2.2122

--- src/main/java/org/cyclops/integrateddynamics/Reference.java
+++ src/main/java/org/cyclops/integrateddynamics/Reference.java
@@ -33,7 +33,7 @@ public final class Reference {
     // MOD ID's
     public static final String MOD_FORGE = "Forge";
     public static final String MOD_FORGE_VERSION = "@FORGE_VERSION@";
-    public static final String MOD_FORGE_VERSION_MIN = "12.18.2.2116";
+    public static final String MOD_FORGE_VERSION_MIN = "12.18.2.2122";
     public static final String MOD_CYCLOPSCORE = "cyclopscore";
     public static final String MOD_CYCLOPSCORE_VERSION = "@CYCLOPSCORE_VERSION@";
     public static final String MOD_CYCLOPSCORE_VERSION_MIN = "0.10.1";
