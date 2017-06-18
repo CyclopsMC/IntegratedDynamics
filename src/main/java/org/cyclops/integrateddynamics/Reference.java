9c4b16b4b7e44b5964f2f4a9ec8e70f564e6c8b8
Ruben Taelman <rubensworks@gmail.com>
2017-06-18 17:20:48 +0200

    Fix Immersive Engineering dependencies

--- src/main/java/org/cyclops/integrateddynamics/Reference.java
+++ src/main/java/org/cyclops/integrateddynamics/Reference.java
@@ -38,7 +38,7 @@ public final class Reference {
     public static final String MOD_CYCLOPSCORE_VERSION = "@CYCLOPSCORE_VERSION@";
     public static final String MOD_CYCLOPSCORE_VERSION_MIN = "0.10.6";
     public static final String MOD_COMMONCAPABILITIES = "commoncapabilities";
-    public static final String MOD_COMMONCAPABILITIES_VERSION_MIN = "1.3.0";
+    public static final String MOD_COMMONCAPABILITIES_VERSION_MIN = "1.3.3";
     public static final String MOD_CHARSETPIPES = "charsetpipes";
     public static final String MOD_MCMULTIPART = "mcmultipart";
     public static final String MOD_WAILA = "Waila";
