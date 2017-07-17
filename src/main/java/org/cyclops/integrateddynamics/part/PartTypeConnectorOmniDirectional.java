afbf715af2415ed4800cd5b6d80e78e28e951f74
Ruben Taelman <rubensworks@gmail.com>
2017-07-17 11:45:53 +0200

    Show omni-connector group id in status bar when right-clicking

--- src/main/java/org/cyclops/integrateddynamics/part/PartTypeConnectorOmniDirectional.java
+++ src/main/java/org/cyclops/integrateddynamics/part/PartTypeConnectorOmniDirectional.java
@@ -4,9 +4,14 @@ import com.google.common.collect.Sets;
 import gnu.trove.map.TIntObjectMap;
 import gnu.trove.map.hash.TIntObjectHashMap;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLServerStartedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -171,6 +176,20 @@ public class PartTypeConnectorOmniDirectional extends PartTypeConnector<PartType
                 tag.setInteger(NBT_KEY_ID, groupId);
             }
         }
+    }
+
+    @Override
+    public boolean onPartActivated(World world, BlockPos pos, State partState, EntityPlayer player, EnumHand hand, ItemStack heldItem, EnumFacing side, float hitX, float hitY, float hitZ) {
+        // Drop through if the player is sneaking
+        if(player.isSneaking() || !partState.isEnabled()) {
+            return false;
+        }
+        if (world.isRemote) {
+            player.sendStatusMessage(new TextComponentTranslation(L10NValues.PART_TOOLTIP_MONODIRECTIONALCONNECTOR_GROUP,
+                    partState.getGroupId()), true);
+        }
+
+        return true;
     }
 
     public static class State extends PartTypeConnector.State<PartTypeConnectorOmniDirectional> {
