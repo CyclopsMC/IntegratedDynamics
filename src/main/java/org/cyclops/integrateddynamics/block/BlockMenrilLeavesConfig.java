8ab7325fbbe48a27fc47c2b396bf2cc67d22911a
Joseph C. Sible <josephcsible@users.noreply.github.com>
2017-10-23 06:26:51 -0400

    Fix some edge cases where additional Menril Tree drops are not collected, Closes #319
    * Fix #319: override the non-deprecated version of getDrops
    
    * Bump CyclopsCore version

--- src/main/java/org/cyclops/integrateddynamics/block/BlockMenrilLeavesConfig.java
+++ src/main/java/org/cyclops/integrateddynamics/block/BlockMenrilLeavesConfig.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.IBlockAccess;
@@ -19,7 +20,6 @@ import org.cyclops.integrateddynamics.IntegratedDynamics;
 import org.cyclops.integrateddynamics.Reference;
 import org.cyclops.integrateddynamics.item.ItemMenrilBerriesConfig;
 
-import java.util.List;
 import java.util.Random;
 
 /**
@@ -62,14 +62,13 @@ public class BlockMenrilLeavesConfig extends BlockConfig {
             }
 
             @Override
-            public List<ItemStack> getDrops(IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
-                List<ItemStack> drops = super.getDrops(world, pos, state, fortune);
+            public void getDrops(NonNullList<ItemStack> drops, IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
+                super.getDrops(drops, world, pos, state, fortune);
                 if(world instanceof World && !((World) world).isRemote) {
                     if(((World) world).rand.nextInt(berriesDropChance) == 0) {
                         drops.add(new ItemStack(ItemMenrilBerriesConfig._instance.getItemInstance()));
                     }
                 }
-                return drops;
             }
 
             @SuppressWarnings("deprecation")
