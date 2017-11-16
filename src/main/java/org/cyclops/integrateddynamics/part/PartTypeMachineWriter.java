814f364cbf5734607aee90cb5b2d21f8b8bece8a
Joseph C. Sible <josephcsible@users.noreply.github.com>
2017-11-15 22:49:05 -0500

    Add an entity and machine writer
    Like the inventory writer, these will only be useful with cross-mod compats

---
+++ src/main/java/org/cyclops/integrateddynamics/part/PartTypeMachineWriter.java
@@ -0,0 +1,28 @@
+package org.cyclops.integrateddynamics.part;
+
+import com.google.common.collect.Lists;
+import org.cyclops.integrateddynamics.api.part.aspect.IAspect;
+import org.cyclops.integrateddynamics.core.part.aspect.AspectRegistry;
+import org.cyclops.integrateddynamics.core.part.write.PartStateWriterBase;
+import org.cyclops.integrateddynamics.core.part.write.PartTypeWriteBase;
+import org.cyclops.integrateddynamics.part.aspect.Aspects;
+
+/**
+ * A machine writer part.
+ * @author josephcsible
+ */
+public class PartTypeMachineWriter extends PartTypeWriteBase<PartTypeMachineWriter, PartStateWriterBase<PartTypeMachineWriter>> {
+
+    public PartTypeMachineWriter(String name) {
+        super(name);
+        AspectRegistry.getInstance().register(this, Lists.<IAspect>newArrayList(
+
+        ));
+    }
+
+    @Override
+    public PartStateWriterBase<PartTypeMachineWriter> constructDefaultState() {
+        return new PartStateWriterBase<PartTypeMachineWriter>(Aspects.REGISTRY.getAspects(this).size());
+    }
+
+}
