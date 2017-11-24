74cbb4d3c841118a3949ac9a3bea1f01d79a48c9
Ruben Taelman <rubensworks@gmail.com>
2017-11-24 16:23:21 +0900

    Use new Cyclops abstractions for rendering the mech squeezer gui

--- src/main/java/org/cyclops/integrateddynamics/client/gui/GuiMechanicalSqueezer.java
+++ src/main/java/org/cyclops/integrateddynamics/client/gui/GuiMechanicalSqueezer.java
@@ -1,11 +1,7 @@
 package org.cyclops.integrateddynamics.client.gui;
 
 import com.google.common.collect.Lists;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.texture.TextureAtlasSprite;
-import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.entity.player.InventoryPlayer;
-import net.minecraft.util.EnumFacing;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.fluids.FluidStack;
 import org.cyclops.cyclopscore.client.gui.component.button.GuiButtonImage;
@@ -13,12 +9,14 @@ import org.cyclops.cyclopscore.client.gui.container.GuiContainerConfigurable;
 import org.cyclops.cyclopscore.client.gui.image.IImage;
 import org.cyclops.cyclopscore.client.gui.image.Image;
 import org.cyclops.cyclopscore.fluid.SingleUseTank;
+import org.cyclops.cyclopscore.helper.GuiHelpers;
 import org.cyclops.cyclopscore.helper.L10NHelpers;
-import org.cyclops.cyclopscore.helper.RenderHelpers;
 import org.cyclops.cyclopscore.item.DamageIndicatedItemComponent;
 import org.cyclops.integrateddynamics.core.helper.L10NValues;
 import org.cyclops.integrateddynamics.inventory.container.ContainerMechanicalSqueezer;
 import org.cyclops.integrateddynamics.tileentity.TileMechanicalSqueezer;
+
+import java.util.Optional;
 
 /**
  * Gui for the mechanical squeezer.
@@ -57,48 +55,19 @@ public class GuiMechanicalSqueezer extends GuiContainerConfigurable<ContainerMec
                 ? imageArrowDownEnabled : imageArrowDownDisabled);
 
         // Render progress
-        int lastMaxProgress = getContainer().getLastMaxProgress();
-        int lastProgress = getContainer().getLastProgress();
-        if (lastMaxProgress > 0) {
-            this.drawTexturedModalRect(getGuiLeft() + 73, getGuiTop() + 36, 176,
-                    120, 12, 18 * lastProgress / lastMaxProgress);
-        }
+        GuiHelpers.renderProgressBar(this, getGuiLeft() + 73, getGuiTop() + 36, 12, 18,
+                176, 120, GuiHelpers.ProgressDirection.DOWN,
+                getContainer().getLastProgress(), getContainer().getLastMaxProgress());
 
         // Render energy level
-        int energyStored = getContainer().getTile().getEnergyStored();
-        int energyMax = getContainer().getTile().getMaxEnergyStored();
-        if (energyMax > 0) {
-            int height = 60 * energyStored / energyMax;
-            this.drawTexturedModalRect(getGuiLeft() + 8, getGuiTop() + 16 + (60 - height), 176,
-                    120 - height, 18, height);
-        }
+        GuiHelpers.renderProgressBar(this, getGuiLeft() + 8, getGuiTop() + 16, 18, 60,
+                176, 60, GuiHelpers.ProgressDirection.UP,
+                getContainer().getTile().getEnergyStored(), getContainer().getTile().getMaxEnergyStored());
 
         // Render fluid tank
-        FluidStack fluidStack = getContainer().getTile().getTank().getFluid();
-        int tankSize = getContainer().getTile().getTank().getCapacity();
-        if (fluidStack != null && tankSize > 0) {
-            int level = 60 * fluidStack.amount / tankSize;
-            TextureAtlasSprite icon = RenderHelpers.getFluidIcon(fluidStack, EnumFacing.UP);
-            int verticalOffset = 0;
-            while(level > 0) {
-                int textureHeight;
-                if(level > 16) {
-                    textureHeight = 16;
-                    level -= 16;
-                } else {
-                    textureHeight = level;
-                    level = 0;
-                }
-
-                mc.renderEngine.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
-                drawTexturedModalRect(getGuiLeft() + 150, getGuiTop() + 10 - textureHeight - verticalOffset + 60, icon, 18, textureHeight);
-                verticalOffset = verticalOffset + 16;
-            }
-
-            GlStateManager.enableBlend();
-            this.mc.renderEngine.bindTexture(texture);
-            this.drawTexturedModalRect(getGuiLeft() + 150, getGuiTop() + 10, 176, 0, 18, 60);
-        }
+        GuiHelpers.renderOverlayedFluidTank(this, getContainer().getTile().getTank().getFluid(),
+                getContainer().getTile().getTank().getCapacity(), getGuiLeft() + 150, getGuiTop() + 10,
+                18, 60, texture, 176, 0);
     }
 
     @Override
@@ -106,38 +75,34 @@ public class GuiMechanicalSqueezer extends GuiContainerConfigurable<ContainerMec
         super.drawGuiContainerForegroundLayer(mouseX, mouseY);
 
         // Draw energy tooltips
-        if(isPointInRegion(8, 16, 18, 60, mouseX, mouseY)) {
+        GuiHelpers.renderTooltipOptional(this, 8, 16, 18, 60, mouseX, mouseY, () -> {
             int energyStored = getContainer().getTile().getEnergyStored();
             int energyMax = getContainer().getTile().getMaxEnergyStored();
             if (energyMax > 0) {
-                drawTooltip(Lists.newArrayList(
+                return Optional.of(Lists.newArrayList(
                         String.format("%,d", energyStored) + " / " + String.format("%,d", energyMax)
-                                + " " + L10NHelpers.localize(L10NValues.GENERAL_ENERGY_UNIT)
-                ), mouseX - getGuiLeft(), mouseY - getGuiTop());
+                                + " " + L10NHelpers.localize(L10NValues.GENERAL_ENERGY_UNIT)));
             }
-        }
+            return Optional.empty();
+        });
 
         // Draw tank tooltips
-        if(isPointInRegion(150, 10, 18, 60, mouseX, mouseY)) {
+        GuiHelpers.renderTooltipOptional(this, 150, 10, 18, 60, mouseX, mouseY, () -> {
             SingleUseTank tank = getContainer().getTile().getTank();
             FluidStack fluidStack = tank.getFluid();
             if (fluidStack != null) {
                 String fluidName = fluidStack.getLocalizedName();
-                drawTooltip(Lists.newArrayList(
-                        fluidName,
-                        DamageIndicatedItemComponent.getInfo(fluidStack, tank.getFluidAmount(), tank.getCapacity())
-                ), mouseX - getGuiLeft(), mouseY - getGuiTop());
+                return Optional.of(Lists.newArrayList(fluidName,
+                        DamageIndicatedItemComponent.getInfo(fluidStack, tank.getFluidAmount(), tank.getCapacity())));
             }
-        }
+            return Optional.empty();
+        });
 
         // Draw fluid auto-eject toggle
-        if(isPointInRegion(150, 70, 18, 10, mouseX, mouseY)) {
-            drawTooltip(Lists.newArrayList(
-                    L10NHelpers.localize(L10NValues.GUI_MECHANICAL_SQUEEZER_TOGGLEFLUIDAUTOEJECT,
-                            TextFormatting.AQUA + L10NHelpers.localize(getContainer().getTile().isAutoEjectFluids() ?
-                                    L10NValues.GENERAL_TRUE : L10NValues.GENERAL_FALSE)),
-                    L10NHelpers.localize(L10NValues.GUI_MECHANICAL_SQUEEZER_TOGGLEFLUIDAUTOEJECT + ".info")
-            ), mouseX - getGuiLeft(), mouseY - getGuiTop());
-        }
+        GuiHelpers.renderTooltip(this, 150, 70, 18, 10, mouseX, mouseY, () -> Lists.newArrayList(
+                L10NHelpers.localize(L10NValues.GUI_MECHANICAL_SQUEEZER_TOGGLEFLUIDAUTOEJECT,
+                        TextFormatting.AQUA + L10NHelpers.localize(getContainer().getTile().isAutoEjectFluids() ?
+                                L10NValues.GENERAL_TRUE : L10NValues.GENERAL_FALSE)),
+                L10NHelpers.localize(L10NValues.GUI_MECHANICAL_SQUEEZER_TOGGLEFLUIDAUTOEJECT + ".info")));
     }
 }
