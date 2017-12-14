c1d80f1a791b55df5f543fd9a718720dd61e5417
Joseph C. Sible <josephcsible@users.noreply.github.com>
2017-12-13 21:38:40 -0500

    Remove some unnecessary stuff

--- src/main/java/org/cyclops/integrateddynamics/core/evaluate/operator/Operators.java
+++ src/main/java/org/cyclops/integrateddynamics/core/evaluate/operator/Operators.java
@@ -77,10 +77,8 @@ import java.util.regex.PatternSyntaxException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
-import java.util.Set;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
