65c02d2926fa6048475132725dbe67e6214437e4
Ruben Taelman <rubensworks@gmail.com>
2017-12-19 10:47:49 +0100

    Balance default energy capacity of batteries
    This multiplies the default capacity by a factor of 10, in order to have a better balance compared to other RF storage mods.

--- src/main/java/org/cyclops/integrateddynamics/block/BlockEnergyBatteryConfig.java
+++ src/main/java/org/cyclops/integrateddynamics/block/BlockEnergyBatteryConfig.java
@@ -26,7 +26,7 @@ public class BlockEnergyBatteryConfig extends BlockContainerConfig {
      * The default capacity of an energy battery.
      */
     @ConfigurableProperty(category = ConfigurableTypeCategory.MACHINE, comment = "The default capacity of an energy battery.", minimalValue = 0)
-    public static int capacity = 100000;
+    public static int capacity = 1000000;
 
     /**
      * The 1/X fraction of the battery capacity that is allowed to be transfered per tick.
@@ -44,12 +44,12 @@ public class BlockEnergyBatteryConfig extends BlockContainerConfig {
      * The maximum capacity possible by combining batteries.
      */
     @ConfigurableProperty(category = ConfigurableTypeCategory.MACHINE, comment = "The maximum capacity possible by combining batteries. (Make sure that you do not cross the max int size.)")
-    public static int maxCapacity = 65536000;
+    public static int maxCapacity = 655360000;
     /**
      * The maximum capacity visible in the creative tabs.
      */
     @ConfigurableProperty(category = ConfigurableTypeCategory.MACHINE, comment = "The maximum capacity visible in the creative tabs. (Make sure that you do not cross the max int size.)")
-    public static int maxCreativeCapacity = 4096000;
+    public static int maxCreativeCapacity = 40960000;
 
     /**
      * Make a new instance.
