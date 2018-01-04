2ef91eff3403bba4796fe4681d8fa9d65964f869
Ruben Taelman <rubensworks@gmail.com>
2018-01-04 12:01:56 +0100

    Add NBT union, intersection and minus operators

--- src/main/java/org/cyclops/integrateddynamics/core/helper/NbtHelpers.java
+++ src/main/java/org/cyclops/integrateddynamics/core/helper/NbtHelpers.java
@@ -1,7 +1,11 @@
 package org.cyclops.integrateddynamics.core.helper;
 
+import com.google.common.collect.Sets;
 import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.common.util.Constants;
+
+import java.util.Set;
 
 /**
  * @author rubensworks
@@ -33,6 +37,77 @@ public class NbtHelpers {
             }
         }
         return true;
+    }
+
+    /**
+     * Create a new NBT tag that contains all entries from the given tags.
+     * If multiple tags contain the same entry, the entry from the latest tag will be given preference.
+     * If nested tags are present, these will be combined recursively.
+     * @param tags NBT tags.
+     * @return A new tag containing the combined entries from the given tags.
+     */
+    public static NBTTagCompound union(NBTTagCompound... tags) {
+        NBTTagCompound tag = new NBTTagCompound();
+        for (NBTTagCompound inputTag : tags) {
+            tag.merge(inputTag);
+        }
+        return tag;
+    }
+
+    /**
+     * Create a new NBT tag that contains the entries that are present in all given tags.
+     * If nested tags are present, these will be intersected recursively.
+     * @param tags NBT tags.
+     * @return A new tag containing the intersected entries from the given tags.
+     */
+    public static NBTTagCompound intersection(NBTTagCompound... tags) {
+        if (tags.length == 0) {
+            return new NBTTagCompound();
+        }
+        NBTTagCompound tag = null;
+        for (NBTTagCompound inputTag : tags) {
+            if (tag == null) {
+                tag = inputTag.copy();
+            } else {
+                Set<String> keys = Sets.newHashSet(tag.getKeySet());
+                for (String key : keys) {
+                    int type = tag.getTag(key).getId();
+                    if (!inputTag.hasKey(key, type)) {
+                        tag.removeTag(key);
+                    } else if (type == Constants.NBT.TAG_COMPOUND) {
+                        tag.setTag(key, intersection(tag.getCompoundTag(key), inputTag.getCompoundTag(key)));
+                    }
+                }
+            }
+        }
+        return tag;
+    }
+
+    /**
+     * Create a new NBT tag that contains all entries of the first tag minus the entries of the second tag.
+     * If nested tags are present, these will be operated recursively.
+     * @param a an NBT tag.
+     * @param b an NBT tag.
+     * @return A new tag containing the entries of a minus b.
+     */
+    public static NBTTagCompound minus(NBTTagCompound a, NBTTagCompound b) {
+        NBTTagCompound tag = a.copy();
+        for (String key : b.getKeySet()) {
+            int type = b.getTag(key).getId();
+            if (tag.hasKey(key, type)) {
+                if (type == Constants.NBT.TAG_COMPOUND) {
+                    NBTTagCompound difference = minus(tag.getCompoundTag(key), b.getCompoundTag(key));
+                    if (difference.hasNoTags()) {
+                        tag.removeTag(key);
+                    } else {
+                        tag.setTag(key, difference);
+                    }
+                } else {
+                    tag.removeTag(key);
+                }
+            }
+        }
+        return tag;
     }
 
 }
