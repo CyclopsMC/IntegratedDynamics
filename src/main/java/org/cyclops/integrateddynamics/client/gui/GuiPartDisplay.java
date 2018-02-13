d278cdf06a20c45dd4f7858e9e56e51e3f38095d
Ruben Taelman <rubensworks@gmail.com>
2018-02-13 20:23:27 +0900

    Add copy functionality to the display panel, Closes #534
    This introduces a new copy button and hotkey to copy the current contents of the display panel to the clipboard of your operating system.

--- src/main/java/org/cyclops/integrateddynamics/client/gui/GuiPartDisplay.java
+++ src/main/java/org/cyclops/integrateddynamics/client/gui/GuiPartDisplay.java
@@ -1,7 +1,14 @@
 package org.cyclops.integrateddynamics.client.gui;
 
+import com.google.common.collect.Lists;
+import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.client.settings.KeyConflictContext;
+import net.minecraftforge.client.settings.KeyModifier;
+import org.cyclops.cyclopscore.client.gui.component.button.GuiButtonText;
+import org.cyclops.cyclopscore.helper.GuiHelpers;
+import org.cyclops.cyclopscore.helper.L10NHelpers;
 import org.cyclops.cyclopscore.helper.RenderHelpers;
 import org.cyclops.integrateddynamics.api.part.IPartContainer;
 import org.cyclops.integrateddynamics.api.part.IPartType;
@@ -9,6 +16,9 @@ import org.cyclops.integrateddynamics.api.part.PartTarget;
 import org.cyclops.integrateddynamics.core.client.gui.container.GuiMultipart;
 import org.cyclops.integrateddynamics.core.part.panel.PartTypePanelVariableDriven;
 import org.cyclops.integrateddynamics.inventory.container.ContainerPartDisplay;
+import org.lwjgl.input.Keyboard;
+
+import java.io.IOException;
 
 
 /**
@@ -22,6 +32,8 @@ public class GuiPartDisplay<P extends PartTypePanelVariableDriven<P, S>, S exten
     private static final int OK_X = 104;
     private static final int OK_Y = 16;
 
+    private static final int BUTTON_COPY = 0;
+
     /**
      * Make a new instance.
      * @param partTarget The target.
@@ -31,6 +43,13 @@ public class GuiPartDisplay<P extends PartTypePanelVariableDriven<P, S>, S exten
      */
     public GuiPartDisplay(EntityPlayer player, PartTarget partTarget, IPartContainer partContainer, IPartType partType) {
         super(new ContainerPartDisplay<P, S>(player, partTarget, partContainer, partType));
+    }
+
+    @Override
+    public void initGui() {
+        super.initGui();
+
+        addButton(new GuiButtonText(BUTTON_COPY, getGuiLeft() + 128, getGuiTop() + 32, 30, 12, L10NHelpers.localize("gui.integrateddynamics.button.copy"), true));
     }
 
     @Override
@@ -61,6 +80,18 @@ public class GuiPartDisplay<P extends PartTypePanelVariableDriven<P, S>, S exten
         super.drawGuiContainerForegroundLayer(mouseX, mouseY);
         // Render error tooltip
         displayErrors.drawForeground(getPartState().getGlobalErrors(), ERROR_X, ERROR_Y, mouseX, mouseY, this, this.guiLeft, this.guiTop);
+
+        // Draw tooltip over copy button
+        GuiHelpers.renderTooltip(this, 128, 32, 30, 12, mouseX, mouseY,
+                () -> Lists.newArrayList(L10NHelpers.localize("gui.integrateddynamics.button.copy.info")));
+    }
+
+    @Override
+    protected void keyTyped(char typedChar, int keyCode) throws IOException {
+        super.keyTyped(typedChar, keyCode);
+        if (Keyboard.KEY_C == keyCode && KeyModifier.CONTROL.isActive(KeyConflictContext.GUI)) {
+            valueToClipboard();
+        }
     }
 
     @Override
@@ -71,5 +102,20 @@ public class GuiPartDisplay<P extends PartTypePanelVariableDriven<P, S>, S exten
     @Override
     protected int getBaseYSize() {
         return 128;
+    }
+
+    @Override
+    protected void actionPerformed(GuiButton button) throws IOException {
+        super.actionPerformed(button);
+        if (button.id == BUTTON_COPY) {
+            valueToClipboard();
+        }
+    }
+
+    protected void valueToClipboard() {
+        String readValue = ((ContainerPartDisplay<?, ?>) getContainer()).getReadValue();
+        if (readValue != null) {
+            setClipboardString(readValue);
+        }
     }
 }
