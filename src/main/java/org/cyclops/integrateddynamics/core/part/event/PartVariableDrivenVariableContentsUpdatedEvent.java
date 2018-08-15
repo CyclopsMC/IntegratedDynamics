748fc152a5247bdf353957ea12c1531803009471
Ruben Taelman <rubensworks@gmail.com>
2018-08-15 11:46:52 +0200

    Loosen types of PartVariableDrivenVariableContentsUpdatedEvent

--- src/main/java/org/cyclops/integrateddynamics/core/part/event/PartVariableDrivenVariableContentsUpdatedEvent.java
+++ src/main/java/org/cyclops/integrateddynamics/core/part/event/PartVariableDrivenVariableContentsUpdatedEvent.java
@@ -5,6 +5,8 @@ import org.cyclops.integrateddynamics.api.evaluate.variable.IValue;
 import org.cyclops.integrateddynamics.api.evaluate.variable.IVariable;
 import org.cyclops.integrateddynamics.api.network.INetwork;
 import org.cyclops.integrateddynamics.api.network.IPartNetwork;
+import org.cyclops.integrateddynamics.api.part.IPartState;
+import org.cyclops.integrateddynamics.api.part.IPartType;
 import org.cyclops.integrateddynamics.api.part.PartTarget;
 import org.cyclops.integrateddynamics.core.part.panel.PartTypePanelVariableDriven;
 
@@ -14,7 +16,7 @@ import javax.annotation.Nullable;
  * An event that is posted in the Forge event bus when the variable contents in a variable-driven part is updated.
  * @author rubensworks
  */
-public class PartVariableDrivenVariableContentsUpdatedEvent<P extends PartTypePanelVariableDriven<P, S>, S extends PartTypePanelVariableDriven.State<P, S>> extends PartEvent<P, S> {
+public class PartVariableDrivenVariableContentsUpdatedEvent<P extends IPartType<P, S>, S extends IPartState<P>> extends PartEvent<P, S> {
 
     @Nullable
     private final EntityPlayer entityPlayer;
