CyclopsMC/IntegratedDynamics

commit 9db2b1a0555cc0eb22168ebf7f589cec78a41e9e

Fix potential energy overflows when inserting energy, Closes #639

src/main/java/org/cyclops/integrateddynamics/core/tileentity/TileMechanicalMachine.java
@@ -313,11 +313,11 @@ public abstract class TileMechanicalMachine<RCK, M extends IMachine<M, I, O, P>,
     protected int extractEnergyInternal(int energy, boolean simulate) {
         energy = Math.max(0, energy);
         int stored = getEnergyStored();
-        int newEnergy = Math.max(stored - energy, 0);
+        int energyReceived = Math.min(getMaxEnergyStored() - stored, energy);
         if(!simulate) {
-            setEnergy(newEnergy);
+            setEnergy(stored + energyReceived);
         }
-        return stored - newEnergy;
+        return energyReceived;
     }
 
     protected void setEnergy(int energy) {

src/main/java/org/cyclops/integrateddynamics/tileentity/TileEnergyBattery.java
@@ -106,11 +106,11 @@ public class TileEnergyBattery extends TileCableConnectable implements IEnergySt
         if(isCreative()) return energy;
         energy = Math.max(0, Math.min(energy, getEnergyPerTick()));
         int stored = getEnergyStored();
-        int newEnergy = Math.max(stored - energy, 0);
+        int energyReceived = Math.min(getMaxEnergyStored() - stored, energy);
         if(!simulate) {
-            setEnergy(newEnergy);
+            setEnergy(stored + energyReceived);
         }
-        return stored - newEnergy;
+        return energyReceived;
     }
 
     protected int addEnergy(int energy) {

src/main/java/org/cyclops/integrateddynamics/capability/energystorage/EnergyStorageItemBlockEnergyContainer.java
@@ -59,11 +59,11 @@ public class EnergyStorageItemBlockEnergyContainer implements IEnergyStorageCapa
     public int receiveEnergy(int energy, boolean simulate) {
         energy = Math.min(energy, getRate());
         int stored = getEnergyStored();
-        int newEnergy = Math.min(stored + energy, getMaxEnergyStored());
+        int energyReceived = Math.min(getMaxEnergyStored() - stored, energy);
         if(!simulate) {
-            setEnergy(itemStack, newEnergy);
+            setEnergy(itemStack, stored + energyReceived);
         }
-        return newEnergy - stored;
+        return energyReceived;
     }
 
     @Override

commit 9735fc3c7c0f33bbb7d5e03891c9b86d39a57ac0

Fix some energy storages incorrectly inserting instead of extracting
Regression since 9db2b1a0555cc0eb22168ebf7f589cec78a41e9e

src/main/java/org/cyclops/integrateddynamics/core/tileentity/TileMechanicalMachine.java
@@ -313,11 +313,11 @@ public abstract class TileMechanicalMachine<RCK, M extends IMachine<M, I, O, P>,
     protected int extractEnergyInternal(int energy, boolean simulate) {
         energy = Math.max(0, energy);
         int stored = getEnergyStored();
-        int energyReceived = Math.min(getMaxEnergyStored() - stored, energy);
+        int newEnergy = Math.max(stored - energy, 0);
         if(!simulate) {
-            setEnergy(stored + energyReceived);
+            setEnergy(newEnergy);
         }
-        return energyReceived;
+        return stored - newEnergy;
     }
 
     protected void setEnergy(int energy) {

src/main/java/org/cyclops/integrateddynamics/tileentity/TileEnergyBattery.java
@@ -106,11 +106,11 @@ public class TileEnergyBattery extends TileCableConnectable implements IEnergySt
         if(isCreative()) return energy;
         energy = Math.max(0, Math.min(energy, getEnergyPerTick()));
         int stored = getEnergyStored();
-        int energyReceived = Math.min(getMaxEnergyStored() - stored, energy);
+        int newEnergy = Math.max(stored - energy, 0);;
         if(!simulate) {
-            setEnergy(stored + energyReceived);
+            setEnergy(newEnergy);
         }
-        return energyReceived;
+        return stored - newEnergy;
     }
 
     protected int addEnergy(int energy) {