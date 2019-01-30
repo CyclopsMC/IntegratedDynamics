12a2beea1e7a32e4d812184364ad6b10f1f51b05
Ruben Taelman <rubensworks@gmail.com>
2019-01-30 19:18:16 +0100

    Don't crash but fix if extraction finalization detected inconsistency
    Closes #679

--- src/main/java/org/cyclops/integrateddynamics/core/network/IngredientChannelAdapter.java
+++ src/main/java/org/cyclops/integrateddynamics/core/network/IngredientChannelAdapter.java
@@ -2,12 +2,14 @@ package org.cyclops.integrateddynamics.core.network;
 
 import com.google.common.collect.Lists;
 import org.apache.commons.lang3.tuple.Pair;
+import org.apache.logging.log4j.Level;
 import org.cyclops.commoncapabilities.api.ingredient.IIngredientMatcher;
 import org.cyclops.commoncapabilities.api.ingredient.IngredientComponent;
 import org.cyclops.commoncapabilities.api.ingredient.storage.IIngredientComponentStorage;
 import org.cyclops.cyclopscore.datastructure.Wrapper;
 import org.cyclops.cyclopscore.ingredient.collection.IIngredientMapMutable;
 import org.cyclops.cyclopscore.ingredient.collection.IngredientHashMap;
+import org.cyclops.integrateddynamics.IntegratedDynamics;
 import org.cyclops.integrateddynamics.api.network.IPartPosIteratorHandler;
 import org.cyclops.integrateddynamics.api.network.IPositionedAddonsNetworkIngredients;
 import org.cyclops.integrateddynamics.api.part.PartPos;
@@ -299,7 +301,10 @@ public abstract class IngredientChannelAdapter<T, M> implements IIngredientCompo
             }
             // Quick heuristic check to see if 'storage' did not lie during its simulation
             if (toExtract != 0) {
-                throw new IllegalStateException("A storage resulted in inconsistent simulated and non-simulated output.");
+                /*IntegratedDynamics.clog(Level.WARN, String.format(
+                        "A storage resulted in inconsistent simulated and non-simulated output. Storages: %s", value.getRight()));*/
+                // This is not such a huge problem actually, so just make sure our output is correct.
+                extractedCount -= toExtract;
             }
         }
         return getComponent().getMatcher().withQuantity(instancePrototype, extractedCount);
