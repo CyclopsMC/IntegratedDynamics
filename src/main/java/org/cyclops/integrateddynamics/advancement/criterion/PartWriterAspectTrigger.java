CyclopsMC/IntegratedDynamics

commit affaab44aa342b1865882af4674375cefe8dfd87

Allow variables in all parts to be referenced
All parts basically act as a Variable Store now.

src/main/java/org/cyclops/integrateddynamics/core/part/PartStateActiveVariableBase.java
@@ -11,14 +11,19 @@ import org.cyclops.cyclopscore.helper.L10NHelpers;
 import org.cyclops.cyclopscore.inventory.SimpleInventory;
 import org.cyclops.cyclopscore.persist.nbt.NBTClassType;
 import org.cyclops.integrateddynamics.IntegratedDynamics;
+import org.cyclops.integrateddynamics.api.block.IVariableContainer;
 import org.cyclops.integrateddynamics.api.evaluate.variable.IValue;
 import org.cyclops.integrateddynamics.api.evaluate.variable.IVariable;
 import org.cyclops.integrateddynamics.api.item.IVariableFacade;
+import org.cyclops.integrateddynamics.api.network.INetwork;
 import org.cyclops.integrateddynamics.api.network.IPartNetwork;
 import org.cyclops.integrateddynamics.api.part.IPartType;
+import org.cyclops.integrateddynamics.api.part.PartPos;
 import org.cyclops.integrateddynamics.api.part.PartTarget;
 import org.cyclops.integrateddynamics.capability.valueinterface.ValueInterfaceConfig;
-import org.cyclops.integrateddynamics.item.ItemVariable;
+import org.cyclops.integrateddynamics.capability.variablecontainer.VariableContainerConfig;
+import org.cyclops.integrateddynamics.capability.variablecontainer.VariableContainerDefault;
+import org.cyclops.integrateddynamics.core.helper.NetworkHelpers;
 
 import java.util.List;
 import java.util.Optional;
@@ -31,6 +36,7 @@ public abstract class PartStateActiveVariableBase<P extends IPartType> extends P
 
     private boolean checkedForWriteVariable = false;
     protected IVariableFacade currentVariableFacade = null;
+    private final IVariableContainer variableContainer;
     @Getter
     @Setter
     private boolean deactivated = false;
@@ -40,6 +46,8 @@ public abstract class PartStateActiveVariableBase<P extends IPartType> extends P
     public PartStateActiveVariableBase(int inventorySize) {
         this.inventory = new SingularInventory(inventorySize);
         this.inventory.addDirtyMarkListener(this); // No need to remove myself eventually. If I am removed, inv is also removed.
+        variableContainer = new VariableContainerDefault();
+        addVolatileCapability(VariableContainerConfig.CAPABILITY, variableContainer);
     }
 
     /**
@@ -76,12 +84,9 @@ public abstract class PartStateActiveVariableBase<P extends IPartType> extends P
      */
     public <V extends IValue> IVariable<V> getVariable(IPartNetwork network) {
         if(!checkedForWriteVariable) {
-            for(int slot = 0; slot < getInventory().getSizeInventory(); slot++) {
-                ItemStack itemStack = getInventory().getStackInSlot(slot);
-                if(!itemStack.isEmpty()) {
-                    this.currentVariableFacade = ItemVariable.getInstance().getVariableFacade(itemStack);
-                    validate(network);
-                }
+            for (IVariableFacade facade : variableContainer.getVariableCache().values()) {
+                currentVariableFacade = facade;
+                validate(network);
             }
             this.checkedForWriteVariable = true;
         }
@@ -103,6 +108,12 @@ public abstract class PartStateActiveVariableBase<P extends IPartType> extends P
         addGlobalError(null);
         this.currentVariableFacade = null;
         //this.deactivated = false; // This *should* not be required anymore, re-activation is handled in AspectWriteBase#update.
+
+        // Refresh any contained variables
+        PartPos center = target.getCenter();
+        INetwork network = NetworkHelpers.getNetwork(center.getPos().getWorld(), center.getPos().getBlockPos(),
+                center.getSide());
+        variableContainer.refreshVariables(network, inventory, false);
     }
 
     /**

src/main/java/org/cyclops/integrateddynamics/core/part/write/PartTypeWriteBase.java
@@ -158,11 +158,14 @@ public abstract class PartTypeWriteBase<P extends IPartTypeWriter<P, S>, S exten
         IAspectWrite aspect = activeIndex == -1 ? null : getWriteAspects().get(activeIndex);
         partState.triggerAspectInfoUpdate((P) this, target, aspect);
 
+        INetwork network = NetworkHelpers.getNetwork(target.getCenter());
         if (aspect != null) {
-            INetwork network = NetworkHelpers.getNetwork(target.getCenter().getPos().getWorld(), target.getCenter().getPos().getBlockPos(), target.getCenter().getSide());
             IPartNetwork partNetwork = NetworkHelpers.getPartNetwork(network);
             MinecraftForge.EVENT_BUS.post(new PartWriterAspectEvent<>(network, partNetwork, target, (P) this, partState, player,
                     aspect, partState.getInventory().getStackInSlot(activeIndex)));
+        }
+        if (network != null) {
+            network.getEventBus().post(new VariableContentsUpdatedEvent(network));
         }
     }
 

src/main/java/org/cyclops/integrateddynamics/inventory/container/ContainerPartDisplay.java
@@ -20,11 +20,12 @@ import org.cyclops.integrateddynamics.api.part.PartTarget;
 import org.cyclops.integrateddynamics.core.helper.NetworkHelpers;
 import org.cyclops.integrateddynamics.core.inventory.container.ContainerMultipart;
 import org.cyclops.integrateddynamics.core.inventory.container.slot.SlotVariable;
+import org.cyclops.integrateddynamics.core.network.event.VariableContentsUpdatedEvent;
 import org.cyclops.integrateddynamics.core.part.event.PartVariableDrivenVariableContentsUpdatedEvent;
 import org.cyclops.integrateddynamics.core.part.panel.PartTypePanelVariableDriven;
 
 /**
- * Container for writer parts.
+ * Container for display parts.
  * @author rubensworks
  */
 @EqualsAndHashCode(callSuper = false)
@@ -90,10 +91,9 @@ public class ContainerPartDisplay<P extends PartTypePanelVariableDriven<P, S>, S
     public void onDirty() {
         if(!MinecraftHelpers.isClientSide()) {
             getPartState().onVariableContentsUpdated(getPartType(), getTarget());
+            INetwork network = NetworkHelpers.getNetwork(getTarget().getCenter());
             if (!getPartState().getInventory().isEmpty()) {
                 try {
-                    INetwork network = NetworkHelpers.getNetwork(getTarget().getCenter().getPos().getWorld(),
-                            getTarget().getCenter().getPos().getBlockPos(), getTarget().getCenter().getSide());
                     IPartNetwork partNetwork = NetworkHelpers.getPartNetwork(network);
                     IVariable variable = getPartState().getVariable(partNetwork);
                     MinecraftForge.EVENT_BUS.post(new PartVariableDrivenVariableContentsUpdatedEvent<>(network, partNetwork, getTarget(),
@@ -101,6 +101,9 @@ public class ContainerPartDisplay<P extends PartTypePanelVariableDriven<P, S>, S
                 } catch (EvaluationException e) {
 
                 }
+            }
+            if (network != null) {
+                network.getEventBus().post(new VariableContentsUpdatedEvent(network));
             }
         }
     }

src/main/java/org/cyclops/integrateddynamics/api/block/IVariableContainer.java
@@ -1,6 +1,8 @@
 package org.cyclops.integrateddynamics.api.block;
 
+import net.minecraft.inventory.IInventory;
 import org.cyclops.integrateddynamics.api.item.IVariableFacade;
+import org.cyclops.integrateddynamics.api.network.INetwork;
 
 import java.util.Map;
 
@@ -14,5 +16,13 @@ public interface IVariableContainer {
      * @return The stored variable facades for this part.
      */
     public Map<Integer, IVariableFacade> getVariableCache();
+
+    /**
+     * Invalidate variables in this cache, clear the cache and re-populate from the supplied inventory
+     * @param network {@link INetwork} that the variables are in
+     * @param inventory IInventory to re-populate the cache from
+     * @param sendVariablesUpdateEvent if true post a VariableContentsUpdatedEvent to the network when done
+     */
+    public void refreshVariables(INetwork network, IInventory inventory, boolean sendVariablesUpdateEvent);
 
 }

src/main/java/org/cyclops/integrateddynamics/capability/variablecontainer/VariableContainerDefault.java
@@ -1,8 +1,16 @@
 package org.cyclops.integrateddynamics.capability.variablecontainer;
 
 import com.google.common.collect.Maps;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.ItemStack;
 import org.cyclops.integrateddynamics.api.block.IVariableContainer;
+import org.cyclops.integrateddynamics.api.evaluate.variable.IVariable;
 import org.cyclops.integrateddynamics.api.item.IVariableFacade;
+import org.cyclops.integrateddynamics.api.network.INetwork;
+import org.cyclops.integrateddynamics.api.network.IPartNetwork;
+import org.cyclops.integrateddynamics.core.helper.NetworkHelpers;
+import org.cyclops.integrateddynamics.core.network.event.VariableContentsUpdatedEvent;
+import org.cyclops.integrateddynamics.item.ItemVariable;
 
 import java.util.Map;
 
@@ -17,5 +25,40 @@ public class VariableContainerDefault implements IVariableContainer {
     @Override
     public Map<Integer, IVariableFacade> getVariableCache() {
         return this.variableCache;
+    }
+
+    @Override
+    public void refreshVariables(INetwork network, IInventory inventory, boolean sendVariablesUpdateEvent){
+        // Invalidate variables
+        IPartNetwork partNetwork = NetworkHelpers.getPartNetwork(network);
+        if (partNetwork != null) {
+            for (IVariableFacade variableFacade : getVariableCache().values()) {
+                IVariable<?> variable = variableFacade.getVariable(partNetwork);
+                if (variable != null) {
+                    if (variable.canInvalidate()) {
+                        variable.invalidate();
+                    }
+                }
+            }
+        }
+
+        // Reset variable facades in inventory
+        getVariableCache().clear();
+        for (int i = 0; i < inventory.getSizeInventory(); i++) {
+            ItemStack itemStack = inventory.getStackInSlot(i);
+            if (!itemStack.isEmpty()) {
+                IVariableFacade variableFacade = ItemVariable.getInstance().getVariableFacade(itemStack);
+                if (variableFacade != null && variableFacade.isValid()) {
+                    getVariableCache().put(variableFacade.getId(), variableFacade);
+                }
+            }
+        }
+
+        // Trigger event in network
+        if (sendVariablesUpdateEvent) {
+            if (network != null) {
+                network.getEventBus().post(new VariableContentsUpdatedEvent(network));
+            }
+        }
     }
 }

src/main/java/org/cyclops/integrateddynamics/core/helper/NetworkHelpers.java
@@ -15,6 +15,7 @@ import org.cyclops.integrateddynamics.api.network.INetworkCarrier;
 import org.cyclops.integrateddynamics.api.network.INetworkElement;
 import org.cyclops.integrateddynamics.api.network.INetworkElementProvider;
 import org.cyclops.integrateddynamics.api.network.IPartNetwork;
+import org.cyclops.integrateddynamics.api.part.PartPos;
 import org.cyclops.integrateddynamics.api.path.IPathElement;
 import org.cyclops.integrateddynamics.capability.network.EnergyNetworkConfig;
 import org.cyclops.integrateddynamics.capability.network.NetworkCarrierConfig;
@@ -68,6 +69,16 @@ public class NetworkHelpers {
             return networkCarrier.getNetwork();
         }
         return null;
+    }
+
+
+    /**
+     * Get the network at the given position.
+     * @param partPos The part position
+     * @return The network, or null if no network or network carrier present.
+     */
+    public static INetwork getNetwork(PartPos partPos) {
+        return NetworkHelpers.getNetwork(partPos.getPos().getWorld(), partPos.getPos().getBlockPos(), partPos.getSide());
     }
 
     /**

src/main/java/org/cyclops/integrateddynamics/core/network/PartNetwork.java
@@ -56,6 +56,7 @@ public class PartNetwork extends FullNetworkListenerAdapter implements IPartNetw
         if(partPositions.containsKey(partId)) {
             return false;
         }
+        compositeVariableCache = null;
         partPositions.put(partId, partPos);
         return true;
     }
@@ -74,6 +75,7 @@ public class PartNetwork extends FullNetworkListenerAdapter implements IPartNetw
 
     @Override
     public void removePart(int partId) {
+        compositeVariableCache = null;
         partPositions.remove(partId);
     }
 
@@ -125,6 +127,14 @@ public class PartNetwork extends FullNetworkListenerAdapter implements IPartNetw
                 } else {
                     IntegratedDynamics.clog(Level.ERROR, "The variable container at " + dimPos + " was invalid, skipping.");
                     it.remove();
+                }
+            }
+            // Also check parts
+            for(PartPos partPos : partPositions.valueCollection()) {
+                IPartContainer partContainer = PartHelpers.getPartContainer(partPos.getPos(), partPos.getSide());
+                IVariableContainer variableContainer = partContainer.getCapability(VariableContainerConfig.CAPABILITY, partPos.getSide());
+                if (variableContainer != null) {
+                    compositeMap.addElement(variableContainer.getVariableCache());
                 }
             }
             compositeVariableCache = compositeMap;

src/main/java/org/cyclops/integrateddynamics/tileentity/TileVariablestore.java
@@ -2,7 +2,6 @@ package org.cyclops.integrateddynamics.tileentity;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
-import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
@@ -12,22 +11,16 @@ import org.cyclops.cyclopscore.datastructure.DimPos;
 import org.cyclops.cyclopscore.helper.MinecraftHelpers;
 import org.cyclops.cyclopscore.persist.IDirtyMarkListener;
 import org.cyclops.integrateddynamics.api.block.IVariableContainer;
-import org.cyclops.integrateddynamics.api.evaluate.variable.IVariable;
-import org.cyclops.integrateddynamics.api.item.IVariableFacade;
-import org.cyclops.integrateddynamics.api.network.INetwork;
 import org.cyclops.integrateddynamics.api.network.INetworkElement;
 import org.cyclops.integrateddynamics.api.network.INetworkEventListener;
-import org.cyclops.integrateddynamics.api.network.IPartNetwork;
 import org.cyclops.integrateddynamics.api.network.event.INetworkEvent;
 import org.cyclops.integrateddynamics.capability.networkelementprovider.NetworkElementProviderConfig;
 import org.cyclops.integrateddynamics.capability.networkelementprovider.NetworkElementProviderSingleton;
 import org.cyclops.integrateddynamics.capability.variablecontainer.VariableContainerConfig;
 import org.cyclops.integrateddynamics.capability.variablecontainer.VariableContainerDefault;
 import org.cyclops.integrateddynamics.capability.variablefacade.VariableFacadeHolderConfig;
-import org.cyclops.integrateddynamics.core.helper.NetworkHelpers;
 import org.cyclops.integrateddynamics.core.network.event.VariableContentsUpdatedEvent;
 import org.cyclops.integrateddynamics.core.tileentity.TileCableConnectableInventory;
-import org.cyclops.integrateddynamics.item.ItemVariable;
 import org.cyclops.integrateddynamics.network.VariablestoreNetworkElement;
 
 import java.util.Collection;
@@ -83,45 +76,14 @@ public class TileVariablestore extends TileCableConnectableInventory
         shouldSendUpdateEvent = true;
     }
 
-    protected void refreshVariables(IInventory inventory, boolean sendVariablesUpdateEvent) {
-        // Invalidate variables
-        IPartNetwork partNetwork = NetworkHelpers.getPartNetwork(getNetwork());
-        if (partNetwork != null) {
-            for (IVariableFacade variableFacade : variableContainer.getVariableCache().values()) {
-                IVariable<?> variable = variableFacade.getVariable(partNetwork);
-                if (variable != null) {
-                    if (variable.canInvalidate()) {
-                        variable.invalidate();
-                    }
-                }
-            }
-        }
-
-        // Reset variable facades
-        variableContainer.getVariableCache().clear();
-        for (int i = 0; i < inventory.getSizeInventory(); i++) {
-            ItemStack itemStack = inventory.getStackInSlot(i);
-            if (!itemStack.isEmpty()) {
-                IVariableFacade variableFacade = ItemVariable.getInstance().getVariableFacade(itemStack);
-                if (variableFacade != null && variableFacade.isValid()) {
-                    variableContainer.getVariableCache().put(variableFacade.getId(), variableFacade);
-                }
-            }
-        }
-
-        // Trigger event in network
-        if (sendVariablesUpdateEvent) {
-            INetwork network = getNetwork();
-            if (network != null) {
-                network.getEventBus().post(new VariableContentsUpdatedEvent(network));
-            }
-        }
+    protected void refreshVariables(boolean sendVariablesUpdateEvent) {
+        variableContainer.refreshVariables(getNetwork(), inventory, sendVariablesUpdateEvent);
     }
 
     @Override
     public void onDirty() {
         if(!world.isRemote) {
-            refreshVariables(inventory, true);
+            refreshVariables(true);
         }
     }
 
@@ -141,7 +103,7 @@ public class TileVariablestore extends TileCableConnectableInventory
         super.updateTileEntity();
         if (shouldSendUpdateEvent && getNetwork() != null) {
             shouldSendUpdateEvent = false;
-            refreshVariables(inventory, true);
+            refreshVariables(true);
         }
     }
 
@@ -158,7 +120,7 @@ public class TileVariablestore extends TileCableConnectableInventory
     @Override
     public void onEvent(INetworkEvent event, VariablestoreNetworkElement networkElement) {
         if(event instanceof VariableContentsUpdatedEvent) {
-            refreshVariables(inventory, false);
+            refreshVariables(false);
         }
     }
 }

commit 7f94236be57dcaa418f33ab9fb32cbd5eb59a475

Partially fix writers not being updated with variable values
Regression since affaab44aa342b1865882af4674375cefe8dfd87

CyclopsMC/IntegratedTunnels#157

src/main/java/org/cyclops/integrateddynamics/advancement/criterion/PartWriterAspectTrigger.java
@@ -60,7 +60,7 @@ public class PartWriterAspectTrigger extends BaseCriterionTrigger<PartWriterAspe
         public boolean test(EntityPlayerMP player, PartWriterAspectEvent event) {
             return (partType == null || event.getPartType() == partType)
                     && (aspect == null || event.getAspect() == aspect)
-                    && variablePredicate.test(((IPartStateWriter) event.getPartState()).getVariable(event.getPartNetwork()));
+                    && variablePredicate.test(((IPartStateWriter) event.getPartState()).getVariable(event.getTarget(), event.getPartNetwork()));
         }
     }
 

src/main/java/org/cyclops/integrateddynamics/api/part/write/IPartStateWriter.java
@@ -33,10 +33,11 @@ public interface IPartStateWriter<P extends IPartTypeWriter> extends IPartState<
      * Get the current target variable.
      * @return The active variable to read from.
      * @param <V> The value type.
+     * @param target The part target.
      * @param network The network this part belongs to.
      * @return The variable.
      */
-    public <V extends IValue> IVariable<V> getVariable(IPartNetwork network);
+    public <V extends IValue> IVariable<V> getVariable(PartTarget target, IPartNetwork network);
 
     /**
      * Indicate that this state should eventually recheck its aspect info because something might have changed what can

src/main/java/org/cyclops/integrateddynamics/core/part/PartStateActiveVariableBase.java
@@ -80,15 +80,24 @@ public abstract class PartStateActiveVariableBase<P extends IPartType> extends P
     /**
      * Get the active variable in this state.
      * @param <V> The variable value type.
-     * @param network The network.
+     * @param target The part target.
+     * @param partNetwork The part network.
      * @return The variable.
      */
-    public <V extends IValue> IVariable<V> getVariable(IPartNetwork network) {
+    public <V extends IValue> IVariable<V> getVariable(PartTarget target, IPartNetwork partNetwork) {
         if(!checkedForWriteVariable) {
+            if (variableContainer.getVariableCache().isEmpty()) {
+                PartPos center = target.getCenter();
+                INetwork network = NetworkHelpers.getNetwork(center.getPos().getWorld(), center.getPos().getBlockPos(),
+                        center.getSide());
+                if (network != null) {
+                    variableContainer.refreshVariables(network, inventory, false);
+                }
+            }
             for (IVariableFacade facade : variableContainer.getVariableCache().values()) {
                 if (facade != null) {
                     currentVariableFacade = facade;
-                    validate(network);
+                    validate(partNetwork);
                 }
             }
             this.checkedForWriteVariable = true;
@@ -97,7 +106,7 @@ public abstract class PartStateActiveVariableBase<P extends IPartType> extends P
             onCorruptedState();
             return null;
         }
-        return currentVariableFacade.getVariable(network);
+        return currentVariableFacade.getVariable(partNetwork);
     }
 
     /**
@@ -165,7 +174,7 @@ public abstract class PartStateActiveVariableBase<P extends IPartType> extends P
         if (capability == ValueInterfaceConfig.CAPABILITY) {
             return ValueInterfaceConfig.CAPABILITY.cast(() -> {
                 if (hasVariable()) {
-                    IVariable<IValue> variable = getVariable(network);
+                    IVariable<IValue> variable = getVariable(target, network);
                     if (variable != null) {
                         return Optional.of(variable.getValue());
                     }

src/main/java/org/cyclops/integrateddynamics/core/part/panel/PartTypePanelVariableDriven.java
@@ -122,7 +122,7 @@ public abstract class PartTypePanelVariableDriven<P extends PartTypePanelVariabl
         IValue newValue = null;
         if(state.hasVariable()) {
             try {
-                IVariable variable = state.getVariable(partNetwork);
+                IVariable variable = state.getVariable(target, partNetwork);
                 if(variable != null) {
                     newValue = variable.getValue();
 
@@ -158,7 +158,7 @@ public abstract class PartTypePanelVariableDriven<P extends PartTypePanelVariabl
 
     @Override
     public <V extends IValue> IVariable<V> getActiveVariable(IPartNetwork network, PartTarget target, S partState) {
-        return partState.getVariable(network);
+        return partState.getVariable(target, network);
     }
 
     protected void onValueChanged(INetwork network, IPartNetwork partNetwork, PartTarget target, S state,

src/main/java/org/cyclops/integrateddynamics/core/part/write/PartTypeWriteBase.java
@@ -137,7 +137,7 @@ public abstract class PartTypeWriteBase<P extends IPartTypeWriter<P, S>, S exten
 
     @Override
     public <V extends IValue> IVariable<V> getActiveVariable(IPartNetwork network, PartTarget target, S partState) {
-        return partState.getVariable(network);
+        return partState.getVariable(target, network);
     }
 
     @Override

src/main/java/org/cyclops/integrateddynamics/inventory/container/ContainerPartDisplay.java
@@ -95,7 +95,7 @@ public class ContainerPartDisplay<P extends PartTypePanelVariableDriven<P, S>, S
             if (!getPartState().getInventory().isEmpty()) {
                 try {
                     IPartNetwork partNetwork = NetworkHelpers.getPartNetwork(network);
-                    IVariable variable = getPartState().getVariable(partNetwork);
+                    IVariable variable = getPartState().getVariable(getTarget(), partNetwork);
                     MinecraftForge.EVENT_BUS.post(new PartVariableDrivenVariableContentsUpdatedEvent<>(network, partNetwork, getTarget(),
                             getPartType(), getPartState(), getPlayer(), variable, variable != null ? variable.getValue() : null));
                 } catch (EvaluationException e) {

src/main/java/org/cyclops/integrateddynamics/inventory/container/ContainerPartWriter.java
@@ -101,7 +101,7 @@ public class ContainerPartWriter<P extends IPartTypeWriter<P, S> & IGuiContainer
                             NetworkHelpers.getNetwork(getPartContainer().getPosition().getWorld(),
                                     getPartContainer().getPosition().getBlockPos(), getTarget().getCenter().getSide()));
                     if (partNetwork != null) {
-                        IVariable variable = getPartState().getVariable(partNetwork);
+                        IVariable variable = getPartState().getVariable(getTarget(), partNetwork);
                         readValue = ValueHelpers.getSafeReadableValue(variable);
                     } else {
                         readValue = Pair.of("NETWORK CORRUPTED!", Helpers.RGBToInt(255, 100, 0));