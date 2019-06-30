CyclopsMC/IntegratedDynamics

commit 378bddb91be040cf29537009d2cf592817c0da00

Expose slotted ingredient networks
This is done through a hierarchical index where all positions are
ordered sequentially and the appropriate position and relative slot
are calculated based on the absolute slot.

This significantly improves performance when external handlers are
interacting with networks, such as hoppers inserting or extracting
items.

Closes CyclopsMC/IntegratedTunnels#167

src/main/java/org/cyclops/integrateddynamics/core/network/IngredientChannelAdapter.java
@@ -1,11 +1,14 @@
 package org.cyclops.integrateddynamics.core.network;
 
+import com.google.common.collect.Iterators;
 import com.google.common.collect.Lists;
 import org.apache.commons.lang3.tuple.Pair;
 import org.cyclops.commoncapabilities.api.ingredient.IIngredientMatcher;
 import org.cyclops.commoncapabilities.api.ingredient.IngredientComponent;
 import org.cyclops.commoncapabilities.api.ingredient.storage.IIngredientComponentStorage;
+import org.cyclops.commoncapabilities.api.ingredient.storage.IIngredientComponentStorageSlotted;
 import org.cyclops.cyclopscore.datastructure.Wrapper;
+import org.cyclops.cyclopscore.helper.Helpers;
 import org.cyclops.cyclopscore.ingredient.collection.IIngredientMapMutable;
 import org.cyclops.cyclopscore.ingredient.collection.IngredientHashMap;
 import org.cyclops.integrateddynamics.api.network.IPartPosIteratorHandler;
@@ -24,7 +27,7 @@ import java.util.function.Supplier;
  * @param <T> The instance type.
  * @param <M> The matching condition parameter.
  */
-public abstract class IngredientChannelAdapter<T, M> implements IIngredientComponentStorage<T, M> {
+public abstract class IngredientChannelAdapter<T, M> implements IIngredientComponentStorageSlotted<T, M> {
 
     private final IPositionedAddonsNetworkIngredients<T, M> network;
     private final int channel;
@@ -339,6 +342,121 @@ public abstract class IngredientChannelAdapter<T, M> implements IIngredientCompo
 
     protected void scheduleObservation() {
         this.network.scheduleObservation();
+    }
+
+    // ----- Expose slots by chaining storages sequentially, not using the index -----
+
+    protected static int getIngredientComponentStorageSize(IIngredientComponentStorage<?, ?> storage) {
+        if (storage instanceof IIngredientComponentStorageSlotted) {
+            return ((IIngredientComponentStorageSlotted<?, ?>) storage).getSlots();
+        } else {
+            return Iterators.size(storage.iterator()) + 1;
+        }
+    }
+
+    @Override
+    public int getSlots() {
+        int slots = 0;
+        IPositionedAddonsNetworkIngredients<T, M> network = getNetwork();
+
+        for (PartPos pos : network.getPositions()) {
+            // Skip if the position is not loaded or disabled
+            if (!pos.getPos().isLoaded() || network.isPositionDisabled(pos)) {
+                continue;
+            }
+            IIngredientComponentStorage<T, M> storage = network.getPositionedStorage(pos);
+            slots = Helpers.addSafe(slots, getIngredientComponentStorageSize(storage));
+        }
+
+        return slots;
+    }
+
+    protected Pair<IIngredientComponentStorage<T, M>, Integer> getStorageAndRelativeSlot(int slot) {
+        IPositionedAddonsNetworkIngredients<T, M> network = getNetwork();
+
+        for (PartPos pos : network.getPositions()) {
+            // Skip if the position is not loaded or disabled
+            if (!pos.getPos().isLoaded() || network.isPositionDisabled(pos)) {
+                continue;
+            }
+            IIngredientComponentStorage<T, M> storage = network.getPositionedStorage(pos);
+            int storageSize = getIngredientComponentStorageSize(storage);
+            if (slot < storageSize) {
+                return Pair.of(storage, slot);
+            } else {
+                slot -= storageSize;
+            }
+        }
+
+        return Pair.of(null, -1);
+    }
+
+    @Override
+    public T getSlotContents(int slotAbsolute) {
+        Pair<IIngredientComponentStorage<T, M>, Integer> storageAndSlot = getStorageAndRelativeSlot(slotAbsolute);
+        IIngredientComponentStorage<T, M> storage = storageAndSlot.getLeft();
+        int slotRelative = storageAndSlot.getRight();
+        if (storage == null) {
+            return getComponent().getMatcher().getEmptyInstance();
+        }
+
+        if (storage instanceof IIngredientComponentStorageSlotted) {
+            return ((IIngredientComponentStorageSlotted<T, M>) storage).getSlotContents(slotRelative);
+        } else {
+            try {
+                return Iterators.get(storage.iterator(), slotRelative);
+            } catch (IndexOutOfBoundsException e) {
+                return getComponent().getMatcher().getEmptyInstance();
+            }
+        }
+    }
+
+    @Override
+    public long getMaxQuantity(int slotAbsolute) {
+        Pair<IIngredientComponentStorage<T, M>, Integer> storageAndSlot = getStorageAndRelativeSlot(slotAbsolute);
+        IIngredientComponentStorage<T, M> storage = storageAndSlot.getLeft();
+        int slotRelative = storageAndSlot.getRight();
+        if (storage == null) {
+            return 0;
+        }
+
+        if (storage instanceof IIngredientComponentStorageSlotted) {
+            return ((IIngredientComponentStorageSlotted<T, M>) storage).getMaxQuantity(slotRelative);
+        } else {
+            return Helpers.castSafe(getComponent().getMatcher().getMaximumQuantity());
+        }
+    }
+
+    @Override
+    public T insert(int slotAbsolute, @Nonnull T ingredient, boolean simulate) {
+        Pair<IIngredientComponentStorage<T, M>, Integer> storageAndSlot = getStorageAndRelativeSlot(slotAbsolute);
+        IIngredientComponentStorage<T, M> storage = storageAndSlot.getLeft();
+        int slotRelative = storageAndSlot.getRight();
+        if (storage == null) {
+            return ingredient;
+        }
+
+        if (storage instanceof IIngredientComponentStorageSlotted) {
+            return ((IIngredientComponentStorageSlotted<T, M>) storage).insert(slotRelative, ingredient, simulate);
+        } else {
+            return storage.insert(ingredient, simulate);
+        }
+    }
+
+    @Override
+    public T extract(int slotAbsolute, long maxQuantity, boolean simulate) {
+        Pair<IIngredientComponentStorage<T, M>, Integer> storageAndSlot = getStorageAndRelativeSlot(slotAbsolute);
+        IIngredientComponentStorage<T, M> storage = storageAndSlot.getLeft();
+        int slotRelative = storageAndSlot.getRight();
+        if (storage == null) {
+            return getComponent().getMatcher().getEmptyInstance();
+        }
+
+        if (storage instanceof IIngredientComponentStorageSlotted) {
+            return ((IIngredientComponentStorageSlotted<T, M>) storage).extract(slotRelative, maxQuantity, simulate);
+        } else {
+            return storage.extract(maxQuantity, simulate);
+        }
     }
 
 }

commit f631edbdd07e2773d3fdfc28ed02d96c2fd4359c

Fix performance issue when importing/exporting in IT
This was caused by slotted ingredient transfer taking preference
over slotless transfer.
With this commit, the slotted channel will only be exposed to externals.

Caused by 378bddb91be040cf29537009d2cf592817c0da00

Closes CyclopsMC/IntegratedTunnels#170

src/main/java/org/cyclops/integrateddynamics/core/network/IngredientChannelAdapter.java
@@ -1,14 +1,11 @@
 package org.cyclops.integrateddynamics.core.network;
 
-import com.google.common.collect.Iterators;
 import com.google.common.collect.Lists;
 import org.apache.commons.lang3.tuple.Pair;
 import org.cyclops.commoncapabilities.api.ingredient.IIngredientMatcher;
 import org.cyclops.commoncapabilities.api.ingredient.IngredientComponent;
 import org.cyclops.commoncapabilities.api.ingredient.storage.IIngredientComponentStorage;
-import org.cyclops.commoncapabilities.api.ingredient.storage.IIngredientComponentStorageSlotted;
 import org.cyclops.cyclopscore.datastructure.Wrapper;
-import org.cyclops.cyclopscore.helper.Helpers;
 import org.cyclops.cyclopscore.ingredient.collection.IIngredientMapMutable;
 import org.cyclops.cyclopscore.ingredient.collection.IngredientHashMap;
 import org.cyclops.integrateddynamics.api.network.IPartPosIteratorHandler;
@@ -27,7 +24,7 @@ import java.util.function.Supplier;
  * @param <T> The instance type.
  * @param <M> The matching condition parameter.
  */
-public abstract class IngredientChannelAdapter<T, M> implements IIngredientComponentStorageSlotted<T, M> {
+public abstract class IngredientChannelAdapter<T, M> implements IIngredientComponentStorage<T, M> {
 
     private final IPositionedAddonsNetworkIngredients<T, M> network;
     private final int channel;
@@ -342,121 +339,6 @@ public abstract class IngredientChannelAdapter<T, M> implements IIngredientCompo
 
     protected void scheduleObservation() {
         this.network.scheduleObservation();
-    }
-
-    // ----- Expose slots by chaining storages sequentially, not using the index -----
-
-    protected static int getIngredientComponentStorageSize(IIngredientComponentStorage<?, ?> storage) {
-        if (storage instanceof IIngredientComponentStorageSlotted) {
-            return ((IIngredientComponentStorageSlotted<?, ?>) storage).getSlots();
-        } else {
-            return Iterators.size(storage.iterator()) + 1;
-        }
-    }
-
-    @Override
-    public int getSlots() {
-        int slots = 0;
-        IPositionedAddonsNetworkIngredients<T, M> network = getNetwork();
-
-        for (PartPos pos : network.getPositions()) {
-            // Skip if the position is not loaded or disabled
-            if (!pos.getPos().isLoaded() || network.isPositionDisabled(pos)) {
-                continue;
-            }
-            IIngredientComponentStorage<T, M> storage = network.getPositionedStorage(pos);
-            slots = Helpers.addSafe(slots, getIngredientComponentStorageSize(storage));
-        }
-
-        return slots;
-    }
-
-    protected Pair<IIngredientComponentStorage<T, M>, Integer> getStorageAndRelativeSlot(int slot) {
-        IPositionedAddonsNetworkIngredients<T, M> network = getNetwork();
-
-        for (PartPos pos : network.getPositions()) {
-            // Skip if the position is not loaded or disabled
-            if (!pos.getPos().isLoaded() || network.isPositionDisabled(pos)) {
-                continue;
-            }
-            IIngredientComponentStorage<T, M> storage = network.getPositionedStorage(pos);
-            int storageSize = getIngredientComponentStorageSize(storage);
-            if (slot < storageSize) {
-                return Pair.of(storage, slot);
-            } else {
-                slot -= storageSize;
-            }
-        }
-
-        return Pair.of(null, -1);
-    }
-
-    @Override
-    public T getSlotContents(int slotAbsolute) {
-        Pair<IIngredientComponentStorage<T, M>, Integer> storageAndSlot = getStorageAndRelativeSlot(slotAbsolute);
-        IIngredientComponentStorage<T, M> storage = storageAndSlot.getLeft();
-        int slotRelative = storageAndSlot.getRight();
-        if (storage == null) {
-            return getComponent().getMatcher().getEmptyInstance();
-        }
-
-        if (storage instanceof IIngredientComponentStorageSlotted) {
-            return ((IIngredientComponentStorageSlotted<T, M>) storage).getSlotContents(slotRelative);
-        } else {
-            try {
-                return Iterators.get(storage.iterator(), slotRelative);
-            } catch (IndexOutOfBoundsException e) {
-                return getComponent().getMatcher().getEmptyInstance();
-            }
-        }
-    }
-
-    @Override
-    public long getMaxQuantity(int slotAbsolute) {
-        Pair<IIngredientComponentStorage<T, M>, Integer> storageAndSlot = getStorageAndRelativeSlot(slotAbsolute);
-        IIngredientComponentStorage<T, M> storage = storageAndSlot.getLeft();
-        int slotRelative = storageAndSlot.getRight();
-        if (storage == null) {
-            return 0;
-        }
-
-        if (storage instanceof IIngredientComponentStorageSlotted) {
-            return ((IIngredientComponentStorageSlotted<T, M>) storage).getMaxQuantity(slotRelative);
-        } else {
-            return Helpers.castSafe(getComponent().getMatcher().getMaximumQuantity());
-        }
-    }
-
-    @Override
-    public T insert(int slotAbsolute, @Nonnull T ingredient, boolean simulate) {
-        Pair<IIngredientComponentStorage<T, M>, Integer> storageAndSlot = getStorageAndRelativeSlot(slotAbsolute);
-        IIngredientComponentStorage<T, M> storage = storageAndSlot.getLeft();
-        int slotRelative = storageAndSlot.getRight();
-        if (storage == null) {
-            return ingredient;
-        }
-
-        if (storage instanceof IIngredientComponentStorageSlotted) {
-            return ((IIngredientComponentStorageSlotted<T, M>) storage).insert(slotRelative, ingredient, simulate);
-        } else {
-            return storage.insert(ingredient, simulate);
-        }
-    }
-
-    @Override
-    public T extract(int slotAbsolute, long maxQuantity, boolean simulate) {
-        Pair<IIngredientComponentStorage<T, M>, Integer> storageAndSlot = getStorageAndRelativeSlot(slotAbsolute);
-        IIngredientComponentStorage<T, M> storage = storageAndSlot.getLeft();
-        int slotRelative = storageAndSlot.getRight();
-        if (storage == null) {
-            return getComponent().getMatcher().getEmptyInstance();
-        }
-
-        if (storage instanceof IIngredientComponentStorageSlotted) {
-            return ((IIngredientComponentStorageSlotted<T, M>) storage).extract(slotRelative, maxQuantity, simulate);
-        } else {
-            return storage.extract(maxQuantity, simulate);
-        }
     }
 
 }

src/main/java/org/cyclops/integrateddynamics/core/network/IngredientChannelAdapterWrapperSlotted.java
@@ -1,0 +1,180 @@
+package org.cyclops.integrateddynamics.core.network;
+
+import com.google.common.collect.Iterators;
+import org.apache.commons.lang3.tuple.Pair;
+import org.cyclops.commoncapabilities.api.ingredient.IngredientComponent;
+import org.cyclops.commoncapabilities.api.ingredient.storage.IIngredientComponentStorage;
+import org.cyclops.commoncapabilities.api.ingredient.storage.IIngredientComponentStorageSlotted;
+import org.cyclops.cyclopscore.helper.Helpers;
+import org.cyclops.integrateddynamics.api.network.IPositionedAddonsNetworkIngredients;
+import org.cyclops.integrateddynamics.api.part.PartPos;
+
+import javax.annotation.Nonnull;
+import java.util.Iterator;
+
+/**
+ * A slotted wrapper over {@link IngredientChannelAdapter}.
+ *
+ * It exposes slots by chaining storages sequentially, not using the index.
+ *
+ * @param <T> The instance type.
+ * @param <M> The matching condition parameter.
+ *
+ * @author rubensworks
+ */
+public class IngredientChannelAdapterWrapperSlotted<T, M> implements IIngredientComponentStorageSlotted<T, M> {
+
+    private final IngredientChannelAdapter<T, M> channel;
+
+    public IngredientChannelAdapterWrapperSlotted(IngredientChannelAdapter<T, M> channel) {
+        this.channel = channel;
+    }
+
+    protected static int getIngredientComponentStorageSize(IIngredientComponentStorage<?, ?> storage) {
+        if (storage instanceof IIngredientComponentStorageSlotted) {
+            return ((IIngredientComponentStorageSlotted<?, ?>) storage).getSlots();
+        } else {
+            return Iterators.size(storage.iterator()) + 1;
+        }
+    }
+
+    @Override
+    public int getSlots() {
+        int slots = 0;
+        IPositionedAddonsNetworkIngredients<T, M> network = this.channel.getNetwork();
+
+        for (PartPos pos : network.getPositions()) {
+            // Skip if the position is not loaded or disabled
+            if (!pos.getPos().isLoaded() || network.isPositionDisabled(pos)) {
+                continue;
+            }
+            IIngredientComponentStorage<T, M> storage = network.getPositionedStorage(pos);
+            slots = Helpers.addSafe(slots, getIngredientComponentStorageSize(storage));
+        }
+
+        return slots;
+    }
+
+    protected Pair<IIngredientComponentStorage<T, M>, Integer> getStorageAndRelativeSlot(int slot) {
+        IPositionedAddonsNetworkIngredients<T, M> network = this.channel.getNetwork();
+
+        for (PartPos pos : network.getPositions()) {
+            // Skip if the position is not loaded or disabled
+            if (!pos.getPos().isLoaded() || network.isPositionDisabled(pos)) {
+                continue;
+            }
+            IIngredientComponentStorage<T, M> storage = network.getPositionedStorage(pos);
+            int storageSize = getIngredientComponentStorageSize(storage);
+            if (slot < storageSize) {
+                return Pair.of(storage, slot);
+            } else {
+                slot -= storageSize;
+            }
+        }
+
+        return Pair.of(null, -1);
+    }
+
+    @Override
+    public T getSlotContents(int slotAbsolute) {
+        Pair<IIngredientComponentStorage<T, M>, Integer> storageAndSlot = getStorageAndRelativeSlot(slotAbsolute);
+        IIngredientComponentStorage<T, M> storage = storageAndSlot.getLeft();
+        int slotRelative = storageAndSlot.getRight();
+        if (storage == null) {
+            return getComponent().getMatcher().getEmptyInstance();
+        }
+
+        if (storage instanceof IIngredientComponentStorageSlotted) {
+            return ((IIngredientComponentStorageSlotted<T, M>) storage).getSlotContents(slotRelative);
+        } else {
+            try {
+                return Iterators.get(storage.iterator(), slotRelative);
+            } catch (IndexOutOfBoundsException e) {
+                return getComponent().getMatcher().getEmptyInstance();
+            }
+        }
+    }
+
+    @Override
+    public long getMaxQuantity(int slotAbsolute) {
+        Pair<IIngredientComponentStorage<T, M>, Integer> storageAndSlot = getStorageAndRelativeSlot(slotAbsolute);
+        IIngredientComponentStorage<T, M> storage = storageAndSlot.getLeft();
+        int slotRelative = storageAndSlot.getRight();
+        if (storage == null) {
+            return 0;
+        }
+
+        if (storage instanceof IIngredientComponentStorageSlotted) {
+            return ((IIngredientComponentStorageSlotted<T, M>) storage).getMaxQuantity(slotRelative);
+        } else {
+            return Helpers.castSafe(getComponent().getMatcher().getMaximumQuantity());
+        }
+    }
+
+    @Override
+    public T insert(int slotAbsolute, @Nonnull T ingredient, boolean simulate) {
+        Pair<IIngredientComponentStorage<T, M>, Integer> storageAndSlot = getStorageAndRelativeSlot(slotAbsolute);
+        IIngredientComponentStorage<T, M> storage = storageAndSlot.getLeft();
+        int slotRelative = storageAndSlot.getRight();
+        if (storage == null) {
+            return ingredient;
+        }
+
+        if (storage instanceof IIngredientComponentStorageSlotted) {
+            return ((IIngredientComponentStorageSlotted<T, M>) storage).insert(slotRelative, ingredient, simulate);
+        } else {
+            return storage.insert(ingredient, simulate);
+        }
+    }
+
+    @Override
+    public T extract(int slotAbsolute, long maxQuantity, boolean simulate) {
+        Pair<IIngredientComponentStorage<T, M>, Integer> storageAndSlot = getStorageAndRelativeSlot(slotAbsolute);
+        IIngredientComponentStorage<T, M> storage = storageAndSlot.getLeft();
+        int slotRelative = storageAndSlot.getRight();
+        if (storage == null) {
+            return getComponent().getMatcher().getEmptyInstance();
+        }
+
+        if (storage instanceof IIngredientComponentStorageSlotted) {
+            return ((IIngredientComponentStorageSlotted<T, M>) storage).extract(slotRelative, maxQuantity, simulate);
+        } else {
+            return storage.extract(maxQuantity, simulate);
+        }
+    }
+
+    @Override
+    public IngredientComponent<T, M> getComponent() {
+        return channel.getComponent();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return channel.iterator();
+    }
+
+    @Override
+    public Iterator<T> iterator(@Nonnull T prototype, M matchCondition) {
+        return channel.iterator(prototype, matchCondition);
+    }
+
+    @Override
+    public long getMaxQuantity() {
+        return channel.getMaxQuantity();
+    }
+
+    @Override
+    public T insert(@Nonnull T ingredient, boolean simulate) {
+        return channel.insert(ingredient, simulate);
+    }
+
+    @Override
+    public T extract(@Nonnull T prototype, M matchCondition, boolean simulate) {
+        return channel.extract(prototype, matchCondition, simulate);
+    }
+
+    @Override
+    public T extract(long maxQuantity, boolean simulate) {
+        return channel.extract(maxQuantity, simulate);
+    }
+}

src/main/java/org/cyclops/integrateddynamics/core/network/PositionedAddonsNetworkIngredients.java
@@ -4,8 +4,10 @@ import com.google.common.collect.Maps;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap;
 import net.minecraft.util.EnumFacing;
+import net.minecraftforge.common.capabilities.Capability;
 import org.cyclops.commoncapabilities.api.ingredient.IngredientComponent;
 import org.cyclops.commoncapabilities.api.ingredient.storage.IIngredientComponentStorage;
+import org.cyclops.commoncapabilities.api.ingredient.storage.IIngredientComponentStorageWrapperHandler;
 import org.cyclops.cyclopscore.ingredient.collection.IIngredientCollection;
 import org.cyclops.integrateddynamics.GeneralConfig;
 import org.cyclops.integrateddynamics.api.ingredient.IIngredientComponentStorageObservable;
@@ -170,6 +172,15 @@ public abstract class PositionedAddonsNetworkIngredients<T, M> extends Positione
             index = new IngredientPositionsIndexEmpty<>(getComponent());
         }
         return index;
+    }
+
+    @Nullable
+    @Override
+    public <S> S getChannelExternal(Capability<S> capability, int channel) {
+        IIngredientComponentStorageWrapperHandler<T, M, S> wrapperHandler = getComponent()
+                .getStorageWrapperHandler(capability);
+        return wrapperHandler != null ? wrapperHandler.wrapStorage(new IngredientChannelAdapterWrapperSlotted<>(
+                (IngredientChannelAdapter<T, M>) getChannel(channel))) : null;
     }
 
     @Override