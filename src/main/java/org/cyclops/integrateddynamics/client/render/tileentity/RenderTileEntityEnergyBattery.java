88cb0e5a4eff4dd07155b1debc017189fbe6f39a
Ruben Taelman <rubensworks@gmail.com>
2020-04-24 20:37:18 +0200

    Fix energy battery overlay not rendering

--- src/main/java/org/cyclops/integrateddynamics/client/render/tileentity/RenderTileEntityEnergyBattery.java
+++ src/main/java/org/cyclops/integrateddynamics/client/render/tileentity/RenderTileEntityEnergyBattery.java
@@ -5,6 +5,7 @@ import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.Matrix4f;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
@@ -72,17 +73,15 @@ public class RenderTileEntityEnergyBattery extends TileEntityRenderer<TileEnergy
 	public void render(TileEnergyBattery tile, float partialTicks, MatrixStack matrixStack,
                        IRenderTypeBuffer renderTypeBuffer, int combinedLight, int combinedOverlay) {
         if(tile != null && tile.getEnergyStored() > 0) {
-            float height = tile.getEnergyStored() / tile.getMaxEnergyStored();
+            float height = (float) tile.getEnergyStored() / tile.getMaxEnergyStored();
 
             // Re-scale height to [0.125, 0.875] range as the energy bar does not take up 100% of the height.
             height = (height * 12 / 16) + 0.125F;
 
-            int l2 = combinedLight >> 0x10 & 0xFFFF;
-            int i3 = combinedLight & 0xFFFF;
-
             matrixStack.push();
 
             for(Direction side : Direction.Plane.HORIZONTAL) {
+                combinedLight = WorldRenderer.getCombinedLight(tile.getWorld(), tile.getPos().add(side.getDirectionVec()));
                 TextureAtlasSprite icon = RegistryEntries.BLOCK_ENERGY_BATTERY.iconOverlay;
 
                 float[][] c = coordinates[side.ordinal()];
@@ -104,10 +103,10 @@ public class RenderTileEntityEnergyBattery extends TileEntityRenderer<TileEnergy
 
                 IVertexBuilder vb = renderTypeBuffer.getBuffer(RenderType.getText(icon.getAtlasTexture().getTextureLocation()));
                 Matrix4f matrix = matrixStack.getLast().getMatrix();
-                vb.pos(matrix, c[0][0], c[0][1] * height, c[0][2]).color(r, g, b, 1).tex(icon.getMinU(), replacedMaxV).lightmap(l2, i3).endVertex();
-                vb.pos(matrix, c[1][0], c[1][1] * height, c[1][2]).color(r, g, b, 1).tex(icon.getMinU(), replacedMinV).lightmap(l2, i3).endVertex();
-                vb.pos(matrix, c[2][0], c[2][1] * height, c[2][2]).color(r, g, b, 1).tex(icon.getMaxU(), replacedMinV).lightmap(l2, i3).endVertex();
-                vb.pos(matrix, c[3][0], c[3][1] * height, c[3][2]).color(r, g, b, 1).tex(icon.getMaxU(), replacedMaxV).lightmap(l2, i3).endVertex();
+                vb.pos(matrix, c[0][0], c[0][1] * height, c[0][2]).color(r, g, b, 1).tex(icon.getMinU(), replacedMaxV).lightmap(combinedLight).endVertex();
+                vb.pos(matrix, c[1][0], c[1][1] * height, c[1][2]).color(r, g, b, 1).tex(icon.getMinU(), replacedMinV).lightmap(combinedLight).endVertex();
+                vb.pos(matrix, c[2][0], c[2][1] * height, c[2][2]).color(r, g, b, 1).tex(icon.getMaxU(), replacedMinV).lightmap(combinedLight).endVertex();
+                vb.pos(matrix, c[3][0], c[3][1] * height, c[3][2]).color(r, g, b, 1).tex(icon.getMaxU(), replacedMaxV).lightmap(combinedLight).endVertex();
             }
 
             matrixStack.pop();
