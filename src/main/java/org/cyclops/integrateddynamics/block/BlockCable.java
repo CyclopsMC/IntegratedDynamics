8de1d99cdb32e82ec0393ed9403c092a9ed37d35
Ruben Taelman <rubensworks@gmail.com>
2020-05-06 09:23:07 +0200

    Support waterlogging for cables

--- src/main/java/org/cyclops/integrateddynamics/block/BlockCable.java
+++ src/main/java/org/cyclops/integrateddynamics/block/BlockCable.java
@@ -4,14 +4,22 @@ import lombok.Setter;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.IWaterLoggable;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.particle.ParticleManager;
 import net.minecraft.client.renderer.model.IBakedModel;
+import net.minecraft.client.renderer.model.ModelResourceLocation;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.Fluids;
 import net.minecraft.fluid.IFluidState;
+import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.ItemStack;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
@@ -24,12 +32,15 @@ import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.Explosion;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
 import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.ModelBakeEvent;
 import net.minecraftforge.client.model.data.ModelProperty;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.cyclops.cyclopscore.block.BlockTile;
 import org.cyclops.cyclopscore.client.icon.Icon;
 import org.cyclops.cyclopscore.client.model.IDynamicModelElement;
@@ -71,10 +82,12 @@ import java.util.Random;
  * This block refers to a ticking part entity.
  * @author rubensworks
  */
-public class BlockCable extends BlockTile implements IDynamicModelElement {
+public class BlockCable extends BlockTile implements IDynamicModelElement, IWaterLoggable {
 
     public static final float BLOCK_HARDNESS = 3.0F;
     public static final Material BLOCK_MATERIAL = Material.GLASS;
+
+    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
 
     // Model Properties
     public static final ModelProperty<Boolean> REALCABLE = new ModelProperty<>();
@@ -117,9 +130,42 @@ public class BlockCable extends BlockTile implements IDynamicModelElement {
 
     public BlockCable(Properties properties) {
         super(properties, TileMultipartTicking::new);
+        this.setDefaultState(this.stateContainer.getBaseState().with(WATERLOGGED, false));
         if (MinecraftHelpers.isClientSide()) {
             IntegratedDynamics._instance.getIconProvider().registerIconHolderObject(this);
         }
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onModelBakeEvent);
+    }
+
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        super.fillStateContainer(builder);
+        builder.add(WATERLOGGED);
+    }
+
+    @Override
+    public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
+        if (stateIn.get(WATERLOGGED)) {
+            worldIn.getPendingFluidTicks().scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickRate(worldIn));
+        }
+        return super.updatePostPlacement(stateIn, facing, facingState, worldIn, currentPos, facingPos);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        IFluidState ifluidstate = context.getWorld().getFluidState(context.getPos());
+        return this.getDefaultState().with(WATERLOGGED, ifluidstate.getFluid() == Fluids.WATER);
+    }
+
+    @Override
+    public IFluidState getFluidState(BlockState state) {
+        return state.get(WATERLOGGED) ? Fluids.WATER.getStillFluidState(false) : super.getFluidState(state);
+    }
+
+    @Override
+    public boolean canContainFluid(IBlockReader worldIn, BlockPos pos, BlockState state, Fluid fluidIn) {
+        return !state.get(BlockStateProperties.WATERLOGGED) && fluidIn == Fluids.WATER
+                && !CableHelpers.hasFacade(worldIn, pos);
     }
 
     @Override
@@ -348,5 +394,11 @@ public class BlockCable extends BlockTile implements IDynamicModelElement {
     @Override
     public IBakedModel createDynamicModel() {
         return new CableModel();
+    }
+
+    public void onModelBakeEvent(ModelBakeEvent event){
+        IBakedModel dynamicModel = this.createDynamicModel();
+        event.getModelRegistry().put(new ModelResourceLocation(getRegistryName(), "waterlogged=false"), dynamicModel);
+        event.getModelRegistry().put(new ModelResourceLocation(getRegistryName(), "waterlogged=true"), dynamicModel);
     }
 }
