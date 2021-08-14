CyclopsMC/IntegratedDynamics

commit 84d56750e3f1815fbf327d6a7e3ab97cf5131ea6

Fix focus issues after labelling within logic programmer
Closes #1061

src/main/java/org/cyclops/integrateddynamics/client/gui/container/ContainerScreenLogicProgrammerBase.java
@@ -18,7 +18,6 @@ import org.cyclops.cyclopscore.client.gui.component.button.ButtonText;
 import org.cyclops.cyclopscore.client.gui.component.input.WidgetTextFieldExtended;
 import org.cyclops.cyclopscore.client.gui.container.ContainerScreenScrolling;
 import org.cyclops.cyclopscore.helper.Helpers;
-import org.cyclops.cyclopscore.helper.L10NHelpers;
 import org.cyclops.cyclopscore.helper.MinecraftHelpers;
 import org.cyclops.cyclopscore.helper.RenderHelpers;
 import org.cyclops.integrateddynamics.IntegratedDynamics;
@@ -438,8 +437,7 @@ public class ContainerScreenLogicProgrammerBase<C extends ContainerLogicProgramm
 
         @Override
         public boolean keyPressed(int typedChar, int keyCode, int modifiers) {
-            if (this.searchField.getVisible() && typedChar != GLFW.GLFW_KEY_ESCAPE) {
-                this.searchField.keyPressed(typedChar, keyCode, modifiers);
+            if (this.searchField.getVisible() && typedChar != GLFW.GLFW_KEY_ESCAPE && this.searchField.keyPressed(typedChar, keyCode, modifiers)) {
                 label(this.searchField.getText());
                 return true;
             }
@@ -448,8 +446,8 @@ public class ContainerScreenLogicProgrammerBase<C extends ContainerLogicProgramm
 
         @Override
         public boolean mouseClicked(double mouseX, double mouseY, int mouseButton) {
-            if(this.searchField.getVisible()) {
-                return this.searchField.mouseClicked(mouseX, mouseY, mouseButton);
+            if(this.searchField.getVisible() && this.searchField.mouseClicked(mouseX, mouseY, mouseButton)) {
+                return true;
             }
             return super.mouseClicked(mouseX, mouseY, mouseButton);
         }

commit d6dbdf4663ef8d5106569423bdd6dfa09853cfcc

Fix not all keys working in LP labeler, Closes #1073
Regression since 84d56750e3f1815fbf327d6a7e3ab97cf5131ea6

src/main/java/org/cyclops/integrateddynamics/client/gui/container/ContainerScreenLogicProgrammerBase.java
@@ -427,7 +427,7 @@ public class ContainerScreenLogicProgrammerBase<C extends ContainerLogicProgramm
 
         @Override
         public boolean charTyped(char typedChar, int keyCode) {
-            if (!this.searchField.getVisible() || !this.searchField.charTyped(typedChar, keyCode)) {
+            if (!this.searchField.isFocused() || !this.searchField.charTyped(typedChar, keyCode)) {
                 return super.charTyped(typedChar, keyCode);
             } else {
                 label(this.searchField.getText());
@@ -437,8 +437,9 @@ public class ContainerScreenLogicProgrammerBase<C extends ContainerLogicProgramm
 
         @Override
         public boolean keyPressed(int typedChar, int keyCode, int modifiers) {
-            if (this.searchField.getVisible() && typedChar != GLFW.GLFW_KEY_ESCAPE && this.searchField.keyPressed(typedChar, keyCode, modifiers)) {
+            if (this.searchField.isFocused() && typedChar != GLFW.GLFW_KEY_ESCAPE) {
                 label(this.searchField.getText());
+                this.searchField.keyPressed(typedChar, keyCode, modifiers);
                 return true;
             }
             return super.keyPressed(typedChar, keyCode, modifiers);