df7db926e5d28115ef3894a85058a96714f9c9c7
Ruben Taelman <rubensworks@gmail.com>
2023-01-15 18:15:45 +0100

    Don't show fluid blocks in creative tab

--- src/main/java/org/cyclops/integrateddynamics/block/BlockFluidMenrilResinConfig.java
+++ src/main/java/org/cyclops/integrateddynamics/block/BlockFluidMenrilResinConfig.java
@@ -3,10 +3,14 @@ package org.cyclops.integrateddynamics.block;
 
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.material.Material;
 import org.cyclops.cyclopscore.config.extendedconfig.BlockConfig;
 import org.cyclops.integrateddynamics.IntegratedDynamics;
+
+import java.util.Collection;
+import java.util.Collections;
 
 /**
  * Config for {@link BlockFluidMenrilResin}.
@@ -24,6 +28,11 @@ public class BlockFluidMenrilResinConfig extends BlockConfig {
                         .strength(100.0F)),
                 (eConfig, block) -> new BlockItem(block, new Item.Properties())
         );
+    }
+
+    @Override
+    protected Collection<ItemStack> defaultCreativeTabEntries() {
+        return Collections.emptyList();
     }
 
 }
