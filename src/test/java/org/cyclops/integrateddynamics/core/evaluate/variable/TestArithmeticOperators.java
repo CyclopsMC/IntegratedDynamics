931b796107037d7837825918adfd68a98351a372
met4000 <nathsharwor@hotmail.com>
2023-08-04 19:48:07 +0800

    adjust tests

--- src/test/java/org/cyclops/integrateddynamics/core/evaluate/variable/TestArithmeticOperators.java
+++ src/test/java/org/cyclops/integrateddynamics/core/evaluate/variable/TestArithmeticOperators.java
@@ -529,4 +529,114 @@ public class TestArithmeticOperators {
         Operators.ARITHMETIC_MINIMUM.evaluate(new IVariable[]{DUMMY_VARIABLE, DUMMY_VARIABLE});
     }
 
+    /**
+     * ----------------------------------- MODULO -----------------------------------
+     */
+
+     @Test
+     public void testArithmeticModulo() throws EvaluationException {
+         IValue res1 = Operators.ARITHMETIC_MODULUS.evaluate(new IVariable[]{i10, i10});
+         assertThat("result is an integer", res1, instanceOf(ValueTypeInteger.ValueInteger.class));
+         assertThat("10 % 10 = 0", ((ValueTypeInteger.ValueInteger) res1).getRawValue(), is(0));
+ 
+         IValue res2 = Operators.ARITHMETIC_MODULUS.evaluate(new IVariable[]{i0, i10});
+         assertThat("0 % 10 = 0", ((ValueTypeInteger.ValueInteger) res2).getRawValue(), is(0));
+ 
+         IValue res3 = Operators.ARITHMETIC_MODULUS.evaluate(new IVariable[]{im10, i10});
+         assertThat("-10 % 10 = 0", ((ValueTypeInteger.ValueInteger) res3).getRawValue(), is(0));
+ 
+         IValue res4 = Operators.ARITHMETIC_MODULUS.evaluate(new IVariable[]{i10, im10});
+         assertThat("10 % -10 = 0", ((ValueTypeInteger.ValueInteger) res4).getRawValue(), is(0));
+ 
+         IValue res5 = Operators.ARITHMETIC_MODULUS.evaluate(new IVariable[]{i10, i15});
+         assertThat("10 % 15 = 0", ((ValueTypeInteger.ValueInteger) res5).getRawValue(), is(10));
+ 
+         IValue res6 = Operators.ARITHMETIC_MODULUS.evaluate(new IVariable[]{i15, i10});
+         assertThat("15 % 10 = 1", ((ValueTypeInteger.ValueInteger) res6).getRawValue(), is(5));
+     }
+ 
+     @Test(expected = EvaluationException.class)
+     public void testArithmeticModuloByZero() throws EvaluationException {
+         Operators.ARITHMETIC_MODULUS.evaluate(new IVariable[]{i10, i0});
+     }
+ 
+     @Test(expected = EvaluationException.class)
+     public void testInvalidInputSizeModuloLarge() throws EvaluationException {
+         Operators.ARITHMETIC_MODULUS.evaluate(new IVariable[]{i0, i0, i0});
+     }
+ 
+     @Test(expected = EvaluationException.class)
+     public void testInvalidInputSizeModuloSmall() throws EvaluationException {
+         Operators.ARITHMETIC_MODULUS.evaluate(new IVariable[]{i0});
+     }
+ 
+     @Test(expected = EvaluationException.class)
+     public void testInvalidInputTypeModulo() throws EvaluationException {
+         Operators.ARITHMETIC_MODULUS.evaluate(new IVariable[]{DUMMY_VARIABLE, DUMMY_VARIABLE});
+     }
+ 
+     /**
+      * ----------------------------------- INCREMENT -----------------------------------
+      */
+ 
+     @Test
+     public void testArithmeticIncrement() throws EvaluationException {
+         IValue res1 = Operators.ARITHMETIC_INCREMENT.evaluate(new IVariable[]{i10});
+         assertThat("result is an integer", res1, instanceOf(ValueTypeInteger.ValueInteger.class));
+         assertThat("10++ = 11", ((ValueTypeInteger.ValueInteger) res1).getRawValue(), is(11));
+ 
+         IValue res2 = Operators.ARITHMETIC_INCREMENT.evaluate(new IVariable[]{i0});
+         assertThat("0++ = 1", ((ValueTypeInteger.ValueInteger) res2).getRawValue(), is(1));
+ 
+         IValue res3 = Operators.ARITHMETIC_INCREMENT.evaluate(new IVariable[]{im10});
+         assertThat("-10++ = -9", ((ValueTypeInteger.ValueInteger) res3).getRawValue(), is(-9));
+     }
+ 
+     @Test(expected = EvaluationException.class)
+     public void testInvalidInputSizeIncrementLarge() throws EvaluationException {
+         Operators.ARITHMETIC_INCREMENT.evaluate(new IVariable[]{i0, i0});
+     }
+ 
+     @Test(expected = EvaluationException.class)
+     public void testInvalidInputSizeIncrementSmall() throws EvaluationException {
+         Operators.ARITHMETIC_INCREMENT.evaluate(new IVariable[]{});
+     }
+ 
+     @Test(expected = EvaluationException.class)
+     public void testInvalidInputTypeIncrement() throws EvaluationException {
+         Operators.ARITHMETIC_INCREMENT.evaluate(new IVariable[]{DUMMY_VARIABLE});
+     }
+ 
+     /**
+      * ----------------------------------- DECREMENT -----------------------------------
+      */
+ 
+     @Test
+     public void testArithmeticDecrement() throws EvaluationException {
+         IValue res1 = Operators.ARITHMETIC_DECREMENT.evaluate(new IVariable[]{i10});
+         assertThat("result is an integer", res1, instanceOf(ValueTypeInteger.ValueInteger.class));
+         assertThat("10-- = 9", ((ValueTypeInteger.ValueInteger) res1).getRawValue(), is(9));
+ 
+         IValue res2 = Operators.ARITHMETIC_DECREMENT.evaluate(new IVariable[]{i0});
+         assertThat("0-- = -1", ((ValueTypeInteger.ValueInteger) res2).getRawValue(), is(-1));
+ 
+         IValue res3 = Operators.ARITHMETIC_DECREMENT.evaluate(new IVariable[]{im10});
+         assertThat("-10-- = -11", ((ValueTypeInteger.ValueInteger) res3).getRawValue(), is(-11));
+     }
+ 
+     @Test(expected = EvaluationException.class)
+     public void testInvalidInputSizeDecrementLarge() throws EvaluationException {
+         Operators.ARITHMETIC_DECREMENT.evaluate(new IVariable[]{i0, i0});
+     }
+ 
+     @Test(expected = EvaluationException.class)
+     public void testInvalidInputSizeDecrementSmall() throws EvaluationException {
+         Operators.ARITHMETIC_DECREMENT.evaluate(new IVariable[]{});
+     }
+ 
+     @Test(expected = EvaluationException.class)
+     public void testInvalidInputTypeDecrement() throws EvaluationException {
+         Operators.ARITHMETIC_DECREMENT.evaluate(new IVariable[]{DUMMY_VARIABLE});
+     }
+
 }
