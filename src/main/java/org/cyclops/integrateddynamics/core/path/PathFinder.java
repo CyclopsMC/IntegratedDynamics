49d365bd85ed59806034cbe1a4344ea8d43712e5
Lssikkes <seniltai@gmail.com>
2025-03-01 06:58:40 +0100

    Improve performance of block changes to very large networks
    Pathfinder is ~70x faster, also improved tick speed with lots of devices (17 ms to 3 ms in test scene)

--- src/main/java/org/cyclops/integrateddynamics/core/path/PathFinder.java
+++ src/main/java/org/cyclops/integrateddynamics/core/path/PathFinder.java
@@ -1,12 +1,15 @@
 package org.cyclops.integrateddynamics.core.path;
 
-import com.google.common.collect.Sets;
 import org.cyclops.cyclopscore.datastructure.DimPos;
 import org.cyclops.integrateddynamics.api.path.IPathElement;
 import org.cyclops.integrateddynamics.api.path.ISidedPathElement;
 
+import java.util.ArrayDeque;
+import java.util.HashSet;
+import java.util.Queue;
 import java.util.Set;
 import java.util.TreeSet;
+
 
 /**
  * Algorithm to construct paths/clusters of {@link IPathElement}s.
@@ -14,35 +17,33 @@ import java.util.TreeSet;
  */
 public final class PathFinder {
 
-    protected static TreeSet<ISidedPathElement> getConnectedElements(ISidedPathElement head, Set<DimPos> visitedPositions) {
-        TreeSet<ISidedPathElement> elements = Sets.newTreeSet();
+    protected static Set<ISidedPathElement> getConnectedElements(ISidedPathElement head) {
+        Set<ISidedPathElement> connectedElements = new HashSet<>();
+        Set<DimPos> visitedPositions = new HashSet<>();
 
-        // Make sure to add our head
-        if(!visitedPositions.contains(head.getPathElement().getPosition())) {
-            elements.add(head);
-            visitedPositions.add(head.getPathElement().getPosition());
-        }
+        // Use a queue for BFS traversal.
+        Queue<ISidedPathElement> queue = new ArrayDeque<>();
+        DimPos headPos = head.getPathElement().getPosition();
+        visitedPositions.add(headPos);
+        queue.offer(head);
 
-        // Add neighbours that haven't been checked yet.
-        for(ISidedPathElement neighbour : head.getPathElement().getReachableElements()) {
-            if(!visitedPositions.contains(neighbour.getPathElement().getPosition())) {
-                elements.add(neighbour);
-                visitedPositions.add(neighbour.getPathElement().getPosition());
+        while (!queue.isEmpty()) {
+            ISidedPathElement current = queue.poll();
+            connectedElements.add(current);
+            IPathElement currentElement = current.getPathElement();
+
+            for (ISidedPathElement neighbour : currentElement.getReachableElements()) {
+                DimPos neighbourPos = neighbour.getPathElement().getPosition();
+                if (visitedPositions.add(neighbourPos)) { // Adds and returns true if not already present.
+                    queue.offer(neighbour);
+                }
             }
         }
 
-        // Loop over the added neighbours to recursively check their neighbours.
-        Set<ISidedPathElement> neighbourElements = Sets.newHashSet();
-        for(ISidedPathElement addedElement : elements) {
-            neighbourElements.addAll(getConnectedElements(addedElement, visitedPositions));
-        }
-        elements.addAll(neighbourElements);
-
-        return elements;
+        return connectedElements;
     }
 
     public static Cluster getConnectedCluster(ISidedPathElement head) {
-        return new Cluster(getConnectedElements(head, Sets.<DimPos>newTreeSet()));
+        return new Cluster(new TreeSet<>(getConnectedElements(head)));
     }
-
 }
