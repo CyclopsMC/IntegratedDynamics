6062bade4c5dad3447e4f1b217713cfd7bde4f43
Ruben Taelman <rubensworks@gmail.com>
2025-08-07 17:47:03 +0200

    Fix recipe overlay not changing on shift

--- src/main/java/org/cyclops/integrateddynamics/client/render/blockentity/SpecialModelRendererVariableOverlay.java
+++ src/main/java/org/cyclops/integrateddynamics/client/render/blockentity/SpecialModelRendererVariableOverlay.java
@@ -7,6 +7,8 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.special.SpecialModelRenderer;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
+import org.apache.commons.lang3.tuple.Pair;
+import org.cyclops.cyclopscore.helper.IModHelpers;
 import org.cyclops.integrateddynamics.RegistryEntries;
 import org.cyclops.integrateddynamics.api.evaluate.variable.ValueDeseralizationContext;
 import org.cyclops.integrateddynamics.api.item.IVariableFacade;
@@ -18,20 +20,21 @@ import java.util.Set;
 /**
  * @author rubensworks
  */
-public class SpecialModelRendererVariableOverlay implements SpecialModelRenderer<ItemStack> {
+public class SpecialModelRendererVariableOverlay implements SpecialModelRenderer<Pair<Boolean, ItemStack>> {
 
     @Override
-    public @Nullable ItemStack extractArgument(ItemStack stack) {
-        return stack;
+    public @Nullable Pair<Boolean, ItemStack> extractArgument(ItemStack stack) {
+        // Store shift state to force re-rendering when shift is held/unheld.
+        return Pair.of(IModHelpers.get().getMinecraftClientHelpers().isShifted(), stack);
     }
 
     @Override
-    public void render(@Nullable ItemStack itemStackIn, ItemDisplayContext displayContext, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight, int packedOverlay, boolean hasFoilType) {
+    public void render(@Nullable Pair<Boolean, ItemStack> stateIn, ItemDisplayContext displayContext, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight, int packedOverlay, boolean hasFoilType) {
         if (displayContext == ItemDisplayContext.GUI) {
-            IVariableFacade variableFacade = RegistryEntries.ITEM_VARIABLE.get().getVariableFacade(ValueDeseralizationContext.ofClient(), itemStackIn);
+            IVariableFacade variableFacade = RegistryEntries.ITEM_VARIABLE.get().getVariableFacade(ValueDeseralizationContext.ofClient(), stateIn.getRight());
             displayContext = ItemDisplayContext.GUI;
             poseStack.translate(0.5F, 0.5F, 0.7F);
-            variableFacade.getClient().renderISTER(itemStackIn, displayContext, poseStack, bufferSource, packedLight, packedOverlay);
+            variableFacade.getClient().renderISTER(stateIn.getRight(), displayContext, poseStack, bufferSource, packedLight, packedOverlay);
         }
     }
 
