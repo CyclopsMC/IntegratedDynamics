CyclopsMC/IntegratedDynamics

commit 9bdd91138f709e7125e5e38b97c1c528dd10c303

Add side param to onNeighborBlockChange

src/main/java/org/cyclops/integrateddynamics/core/network/PartNetworkElement.java
@@ -228,6 +228,11 @@ public class PartNetworkElement<P extends IPartType<P, S>, S extends IPartState<
     }
 
     @Override
+    public void onNeighborBlockChange(@Nullable INetwork network, BlockGetter world, @Nullable Direction side) {
+        part.onBlockNeighborChange(network, NetworkHelpers.getPartNetworkChecked(network), getTarget(), getPartState(), world, side);
+    }
+
+    @Override
     public Optional<P> getNetworkEventListener() {
         return Optional.of(getPart());
     }

src/main/java/org/cyclops/integrateddynamics/api/network/INetworkElement.java
@@ -113,7 +113,23 @@ public interface INetworkElement extends Comparable<INetworkElement> {
      * @param network The network to update in.
      * @param world   The world in which the neighbour was updated.
      */
+    @Deprecated // TODO: rm in next major
     public void onNeighborBlockChange(@Nullable INetwork network, BlockGetter world);
+
+    /**
+     * Called when a neighbouring block is updated, more specifically when
+     * {@link Block#neighborChanged(BlockState, Level, BlockPos, Block, Orientation, boolean)},
+     * {@link Block#onNeighborChange(BlockState, LevelReader, BlockPos, BlockPos)}
+     * or {@link Block#updateShape(BlockState, LevelReader, ScheduledTickAccess, BlockPos, Direction, BlockPos, BlockState, RandomSource)} is called.
+     *
+     * @param network The network to update in.
+     * @param world   The world in which the neighbour was updated.
+     * @param side    The side at the center block.
+     */
+    // TODO: rm default impl in next major
+    public default void onNeighborBlockChange(@Nullable INetwork network, BlockGetter world, @Nullable Direction side) {
+
+    }
 
     /**
      * Set the priority and channel of this element in the network.

src/main/java/org/cyclops/integrateddynamics/api/part/IPartType.java
@@ -372,8 +372,28 @@ public interface IPartType<P extends IPartType<P, S>, S extends IPartState<P>> e
      * @param state       The state
      * @param world       The world in which the neighbour was updated.
      */
+    @Deprecated // TODO: rm in next major
     public void onBlockNeighborChange(@Nullable INetwork network, @Nullable IPartNetwork partNetwork, PartTarget target,
                                       S state, BlockGetter world);
+
+    /**
+     * Called when a neighbouring block is updated, more specifically when
+     * {@link Block#neighborChanged(BlockState, Level, BlockPos, Block, Orientation, boolean)},
+     * {@link Block#onNeighborChange(BlockState, LevelReader, BlockPos, BlockPos)}
+     * or {@link Block#updateShape(BlockState, LevelReader, ScheduledTickAccess, BlockPos, Direction, BlockPos, BlockState, RandomSource)} is called.
+     *
+     * @param network     The network to update in.
+     * @param partNetwork The part network to update in.
+     * @param target      The target block.
+     * @param state       The state
+     * @param world       The world in which the neighbour was updated.
+     * @param side        The side at the center block.
+     */
+    // TODO: rm default impl in next major
+    public default void onBlockNeighborChange(@Nullable INetwork network, @Nullable IPartNetwork partNetwork, PartTarget target,
+                                              S state, BlockGetter world, @Nullable Direction side) {
+
+    }
 
     /**
      * @param state The state

src/main/java/org/cyclops/integrateddynamics/api/part/PartTypeAdapter.java
@@ -282,6 +282,12 @@ public abstract class PartTypeAdapter<P extends IPartType<P, S>, S extends IPart
     }
 
     @Override
+    public void onBlockNeighborChange(INetwork network, IPartNetwork partNetwork, PartTarget target, S state,
+                                      BlockGetter world, @Nullable Direction side) {
+
+    }
+
+    @Override
     public int getConsumptionRate(S state) {
         return 0;
     }

src/main/java/org/cyclops/integrateddynamics/block/BlockCable.java
@@ -253,7 +253,7 @@ public class BlockCable extends BlockWithEntity implements SimpleWaterloggedBloc
     @Override
     protected void neighborChanged(BlockState state, Level level, BlockPos pos, Block neighborBlock, @org.jetbrains.annotations.Nullable Orientation orientation, boolean movedByPiston) {
         super.neighborChanged(state, level, pos, neighborBlock, orientation, movedByPiston);
-        NetworkHelpers.onElementProviderBlockNeighborChange(level, pos, null);
+        NetworkHelpers.onElementProviderBlockNeighborChange(level, pos, orientation != null ? orientation.getFront().getOpposite() : null);
     }
 
     @Override

src/main/java/org/cyclops/integrateddynamics/core/helper/NetworkHelpers.java
@@ -263,7 +263,7 @@ public class NetworkHelpers {
             getNetwork(world, pos, side).ifPresent(network -> {
                 getNetworkElementProvider(world, pos, side).ifPresent(networkElementProvider -> {
                     for (INetworkElement networkElement : networkElementProvider.createNetworkElements(world, pos)) {
-                        networkElement.onNeighborBlockChange(network, world);
+                        networkElement.onNeighborBlockChange(network, world, side);
                     }
                 });
             });

commit f57cb9ce5ff046cac9d69c3ff7cb51f69c5c5aa6

Fix broken block-update-based aspect invalidation
Was caused by 9bdd91138f709e7125e5e38b97c1c528dd10c303

src/main/java/org/cyclops/integrateddynamics/core/network/PartNetworkElement.java
@@ -224,7 +224,7 @@ public class PartNetworkElement<P extends IPartType<P, S>, S extends IPartState<
 
     @Override
     public void onNeighborBlockChange(@Nullable INetwork network, BlockGetter world) {
-        part.onBlockNeighborChange(network, NetworkHelpers.getPartNetworkChecked(network), getTarget(), getPartState(), world);
+        part.onBlockNeighborChange(network, NetworkHelpers.getPartNetworkChecked(network), getTarget(), getPartState(), world, null);
     }
 
     @Override

src/main/java/org/cyclops/integrateddynamics/core/part/read/PartTypeReadBase.java
@@ -101,8 +101,8 @@ public abstract class PartTypeReadBase<P extends IPartTypeReader<P, S>, S extend
 
     @Override
     public void onBlockNeighborChange(INetwork network, IPartNetwork partNetwork, PartTarget target, S state,
-                                      BlockGetter world) {
-        super.onBlockNeighborChange(network, partNetwork, target, state, world);
+                                      BlockGetter world, @Nullable Direction side) {
+        super.onBlockNeighborChange(network, partNetwork, target, state, world, side);
         for(IAspect aspect : getUpdateAspects(AspectUpdateType.BLOCK_UPDATE)) {
             aspect.update(network, partNetwork, this, target, state);
         }

src/main/java/org/cyclops/integrateddynamics/part/PartTypePanelLightDynamic.java
@@ -1,6 +1,7 @@
 package org.cyclops.integrateddynamics.part;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
@@ -24,6 +25,8 @@ import org.cyclops.integrateddynamics.core.block.IgnoredBlockStatus;
 import org.cyclops.integrateddynamics.core.evaluate.variable.ValueTypeLightLevels;
 import org.cyclops.integrateddynamics.core.helper.L10NValues;
 import org.cyclops.integrateddynamics.core.part.panel.PartTypePanelVariableDriven;
+
+import javax.annotation.Nullable;
 
 /**
  * A part that can display variables.
@@ -100,8 +103,8 @@ public class PartTypePanelLightDynamic extends PartTypePanelVariableDriven<PartT
 
     @Override
     public void onBlockNeighborChange(INetwork network, IPartNetwork partNetwork, PartTarget target, State state,
-                                      BlockGetter world) {
-        super.onBlockNeighborChange(network, partNetwork, target, state, world);
+                                      BlockGetter world, @Nullable Direction side) {
+        super.onBlockNeighborChange(network, partNetwork, target, state, world, side);
         setLightLevel(target, state.getDisplayValue() == null ? 0 : getLightLevel(state, state.getDisplayValue()));
     }
 

src/main/java/org/cyclops/integrateddynamics/part/PartTypePanelLightStatic.java
@@ -1,5 +1,6 @@
 package org.cyclops.integrateddynamics.part;
 
+import net.minecraft.core.Direction;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -11,6 +12,8 @@ import org.cyclops.integrateddynamics.api.part.PartTarget;
 import org.cyclops.integrateddynamics.core.block.IgnoredBlock;
 import org.cyclops.integrateddynamics.core.part.PartStateEmpty;
 import org.cyclops.integrateddynamics.core.part.panel.PartTypePanel;
+
+import javax.annotation.Nullable;
 
 /**
  * A panel part that simply emits light.
@@ -52,8 +55,8 @@ public class PartTypePanelLightStatic extends PartTypePanel<PartTypePanelLightSt
 
     @Override
     public void onBlockNeighborChange(INetwork network, IPartNetwork partNetwork, PartTarget target,
-                                      PartStateEmpty<PartTypePanelLightStatic> state, BlockGetter world) {
-        super.onBlockNeighborChange(network, partNetwork, target, state, world);
+                                      PartStateEmpty<PartTypePanelLightStatic> state, BlockGetter world, @Nullable Direction side) {
+        super.onBlockNeighborChange(network, partNetwork, target, state, world, side);
         PartTypePanelLightDynamic.setLightLevel(target, LIGHT_LEVEL);
     }
 