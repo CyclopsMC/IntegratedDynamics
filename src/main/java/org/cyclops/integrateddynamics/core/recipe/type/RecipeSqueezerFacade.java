154f61b87bc05a596b9b0b2cc649c547086aafbe
kirjorjos <35607697+kirjorjos@users.noreply.github.com>
2025-12-23 03:03:21 -0500

    Add squeezer recipes for clearing facades

---
+++ src/main/java/org/cyclops/integrateddynamics/core/recipe/type/RecipeSqueezerFacade.java
@@ -0,0 +1,50 @@
+package org.cyclops.integrateddynamics.core.recipe.type;
+
+import org.cyclops.cyclopscore.recipe.ItemStackFromIngredient;
+import org.cyclops.integrateddynamics.RegistryEntries;
+import org.cyclops.integrateddynamics.item.ItemFacade;
+
+import com.mojang.datafixers.util.Either;
+
+import net.minecraft.core.NonNullList;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.Container;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.fluids.FluidStack;
+
+public class RecipeSqueezerFacade extends RecipeSqueezer {
+
+    private static IngredientChance facadeItemChance = new IngredientChance(Either.<ItemStack, ItemStackFromIngredient>left(new ItemStack(RegistryEntries.ITEM_FACADE)), 1.0f);
+
+    public RecipeSqueezerFacade(ResourceLocation id, Ingredient inputIngredient, NonNullList<IngredientChance> outputItems, FluidStack outputFluid) {
+        super(id, inputIngredient, outputItems, outputFluid);
+    }
+
+    @Override
+    public NonNullList<IngredientChance> assemble(ItemStack inputItem) {
+        return getOutput(inputItem);
+    }
+
+    public static NonNullList<IngredientChance> getOutput(ItemStack inputItem) {
+        ItemFacade inputFacade = (ItemFacade)inputItem.getItem();
+        ItemStack facadeBlockItem = inputFacade.getFacadeBlockItem(inputItem);
+
+        if (facadeBlockItem == null) return NonNullList.of(facadeItemChance, facadeItemChance);
+        Either<ItemStack, ItemStackFromIngredient> inputItemEither = Either.left(facadeBlockItem);
+        IngredientChance outputItemChance = new IngredientChance(inputItemEither, 1.0f);
+        return NonNullList.of(outputItemChance, outputItemChance, facadeItemChance);
+    }
+
+    @Override
+    public boolean matches(Container inv, Level worldIn) {
+        return ((inv.getItem(0).getItem() instanceof ItemFacade) && (inv.getItem(0).hasTag()));
+    }
+
+    @Override
+    public RecipeSerializer<?> getSerializer() {
+        return RegistryEntries.RECIPESERIALIZER_SQUEEZER_FACADE;
+    }
+}
