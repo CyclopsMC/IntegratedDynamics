d6b79617f214750620676c19c841d8af7b1b4d95
Ruben Taelman <rubensworks@gmail.com>
2026-01-21 06:04:59 +0100

    Fix missing capability invalidation on modified parts, #1594

--- src/main/java/org/cyclops/integrateddynamics/core/network/PartNetworkElement.java
+++ src/main/java/org/cyclops/integrateddynamics/core/network/PartNetworkElement.java
@@ -114,7 +114,7 @@ public class PartNetworkElement<P extends IPartType<P, S>, S extends IPartState<
     public void revalidate(INetwork network) {
         super.revalidate(network);
         if (revalidatePositioned(network, this.center.getPos())) {
-            part.afterNetworkReAlive(network, NetworkHelpers.getPartNetworkChecked(network), PartTarget.fromCenter(this.center), getPartState());
+            afterNetworkReAlive(network);
         }
     }
 
@@ -195,6 +195,7 @@ public class PartNetworkElement<P extends IPartType<P, S>, S extends IPartState<
     @Override
     public void afterNetworkReAlive(INetwork network) {
         part.afterNetworkReAlive(network, NetworkHelpers.getPartNetworkChecked(network), getTarget(), getPartState());
+        this.center.getPos().getLevel(true).invalidateCapabilities(this.center.getPos().getBlockPos());
     }
 
     @Override
@@ -214,6 +215,7 @@ public class PartNetworkElement<P extends IPartType<P, S>, S extends IPartState<
         boolean res = partNetwork.addPart(getPartState().getId(), this.center);
         if(res) {
             part.onNetworkAddition(network, partNetwork, getTarget(), getPartState());
+            this.center.getPos().getLevel(true).invalidateCapabilities(this.center.getPos().getBlockPos());
         }
         return res;
     }
@@ -242,6 +244,7 @@ public class PartNetworkElement<P extends IPartType<P, S>, S extends IPartState<
     public void onPostRemoved(INetwork network) {
         part.onPostRemoved(network, NetworkHelpers.getPartNetworkChecked(network), getTarget(tempState), Objects.requireNonNull(tempState));
         tempState = null;
+        this.center.getPos().getLevel(true).invalidateCapabilities(this.center.getPos().getBlockPos());
     }
 
     @Override
