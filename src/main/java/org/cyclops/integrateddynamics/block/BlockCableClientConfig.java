4eb59606659b5b95ae8440bfb53ee8572ce7fd4a
Ruben Taelman <rubensworks@gmail.com>
2026-07-31 20:20:02 +0200

    Fix facade rendering being incompatible with ModernFix dynamic resources
    Closes #1690

--- src/main/java/org/cyclops/integrateddynamics/block/BlockCableClientConfig.java
+++ src/main/java/org/cyclops/integrateddynamics/block/BlockCableClientConfig.java
@@ -9,6 +9,8 @@ import net.minecraft.client.renderer.block.dispatch.BlockStateModel;
 import net.minecraft.client.renderer.item.ItemModel;
 import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.resources.model.ModelBaker;
+import net.minecraft.client.resources.model.ModelDebugName;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.Identifier;
@@ -16,11 +18,14 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
+import net.neoforged.neoforge.client.event.ModelEvent;
 import net.neoforged.neoforge.client.event.RegisterColorHandlersEvent;
 import net.neoforged.neoforge.client.event.RegisterItemModelsEvent;
 import net.neoforged.neoforge.client.event.TextureAtlasStitchedEvent;
 import net.neoforged.neoforge.client.extensions.common.IClientBlockExtensions;
 import net.neoforged.neoforge.client.extensions.common.RegisterClientExtensionsEvent;
+import net.neoforged.neoforge.client.model.standalone.StandaloneModelKey;
+import net.neoforged.neoforge.client.model.standalone.UnbakedStandaloneModel;
 import org.apache.commons.lang3.tuple.Pair;
 import org.cyclops.cyclopscore.client.model.IDynamicModelElementCommon;
 import org.cyclops.cyclopscore.config.extendedconfig.BlockClientConfig;
@@ -47,6 +52,7 @@ public class BlockCableClientConfig extends BlockClientConfig<IntegratedDynamics
         blockConfig.getMod().getModEventBus().addListener(this::registerClientExtensions);
         blockConfig.getMod().getModEventBus().addListener(this::postTextureStitch);
         blockConfig.getMod().getModEventBus().addListener((RegisterItemModelsEvent event) -> event.register(Identifier.fromNamespaceAndPath(Reference.MOD_ID, "cable"), ItemModelCable.Unbaked.MAP_CODEC));
+        blockConfig.getMod().getModEventBus().addListener((ModelEvent.RegisterStandalone event) -> event.register(new StandaloneModelKey<>(() -> Reference.MOD_ID + ":CableModelHack"), new StandaloneCableModel()));
     }
 
     public void onRegisterColors(RegisterColorHandlersEvent.BlockTintSources event) {
@@ -115,6 +121,22 @@ public class BlockCableClientConfig extends BlockClientConfig<IntegratedDynamics
             Identifier registryName = BuiltInRegistries.BLOCK.getKey(RegistryEntries.BLOCK_CABLE.get());
             modelConsumer.accept(Pair.of(registryName, model));
             return model;
+        }
+    }
+
+    // This class is only necessary to gain access to the ModelBaker,
+    // since it's needed at CableModel.MODEL_BAKER.
+    // Yes, this is a hack, and may be possible to do in a cleaner way, but would probably require a CableModel rewrite.
+    public static class StandaloneCableModel implements UnbakedStandaloneModel<Object> {
+        @Override
+        public void resolveDependencies(Resolver resolver) {
+            // Do nothing
+        }
+
+        @Override
+        public Object bake(ModelBaker baker, ModelDebugName name) {
+            CableModel.MODEL_BAKER = baker;
+            return null;
         }
     }
 }
